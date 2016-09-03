08b3aa2be5b224d1b4e5f8a1b846e17047ae9a35
Antoine Carme <antoine.carme@laposte.net>
2016-09-03 19:34:05 +0200

    Some benchmarking debugging. (WIP)

--- tests/test_artificial_one_dataset.py
+++ tests/test_artificial_one_dataset.py
@@ -5,19 +5,23 @@ import Bench.Artificial as art
 import warnings
 
 
+H = 2;
+
 with warnings.catch_warnings():
     warnings.simplefilter("error")
     tester1 = art.cArtificial_Tester(tsds.load_artificial_datsets("S") , "ARTIFICIAL_S");
-    tester1.run_multiprocessed(18);
+    tester1.testSignals("Signal_5_D_0_poly_30__0" , H);
+    # tester1.run_multiprocessed(18);
     del tester1;
     tester2 = art.cArtificial_Tester(tsds.load_artificial_datsets("M") , "ARTIFICIAL_M");
-    tester2.run_multiprocessed(18);
+    tester2.testSignals("Signal_400_D_0_linear_0__2" , H);
+    # tester2.run_multiprocessed(18);
     del tester2;
     tester3 = art.cArtificial_Tester(tsds.load_artificial_datsets("L") , "ARTIFICIAL_L");
-    tester3.run_multiprocessed(18);
+    tester3.testSignals("Signal_700_D_0_poly_15_exp_0", H);
+    # tester3.run_multiprocessed(18);
     del tester3;
     tester4 = art.cArtificial_Tester(tsds.load_artificial_datsets("XL") , "ARTIFICIAL_XL");
-    tester4.run_multiprocessed(18);
+    tester4.testSignals("Signal_3000_D_0_poly_60_exp_2", H);
+    # tester4.run_multiprocessed(18);
     del tester4;
-    
-                        
