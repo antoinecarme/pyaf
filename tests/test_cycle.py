cb4df0568fe1eb1c90288f77d91997637cd7f9bb
Antoine Carme <antoine.carme@laposte.net>
2016-09-06 23:46:45 +0200

    Soem tests for debugging cycles.

---
+++ tests/test_cycle.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import AutoForecast as autof
+import Bench.TS_datasets as tsds
+
+#get_ipython().magic('matplotlib inline')
+
+b1 = tsds.generate_random_TS(N = 320 , FREQ = 'D', seed = 0, trendtype = "constant", cycle_length = 12, transform = "", sigma = 0.0);
+df = b1.mPastData
+
+#df.tail(10)
+#df[:-10].tail()
+#df[:-10:-1]
+#df.describe()
+
+
+lAutoF = autof.cAutoForecast()
+lAutoF
+
+H = b1.mHorizon;
+lAutoF.train(df , b1.mTimeVar , b1.mSignalVar, H);
+lAutoF.getModelInfo();
+
+lAutoF.mSignalDecomposition.mBestTransformation.mTimeInfo.mResolution
+
+dfapp_in = df.copy();
+dfapp_in.tail()
+
+#H = 12
+dfapp_out = lAutoF.forecast(dfapp_in, H);
+dfapp_out.tail(2 * H)
+print("Forecast Columns " , dfapp_out.columns);
+Forecast_DF = dfapp_out[[b1.mTimeVar , b1.mSignalVar, b1.mSignalVar + '_BestModelForecast']]
+print(Forecast_DF.info())
+print("Forecasts\n" , Forecast_DF.tail(H).values);
+
+print("\n\n<ModelInfo>")
+print(lAutoF.to_json());
+print("</ModelInfo>\n\n")
+print("\n\n<Forecast>")
+print(Forecast_DF.to_json(date_format='iso'))
+print("</Forecast>\n\n")
---
+++ tests/test_lin_trend_cycle_code_gen.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import AutoForecast as autof
+import Bench.TS_datasets as tsds
+import TS_CodeGen_Objects as tscodegen
+
+#get_ipython().magic('matplotlib inline')
+
+b1 = tsds.generate_random_TS(N = 3200 , FREQ = 'D', seed = 0, trendtype = "linear", cycle_length = 12, transform = "", sigma = 0.0);
+df = b1.mPastData
+df.to_csv("acfrefefs_cycle.csv")
+#df.tail(10)
+#df[:-10].tail()
+#df[:-10:-1]
+#df.describe()
+
+H = b1.mHorizon;
+
+N = df.shape[0];
+for n in range(24*H,  N , 10):
+    df1 = df.head(n).copy();
+    lAutoF = autof.cAutoForecast()
+    lAutoF.mOptions.mEnableSeasonals = False;
+    lAutoF.mOptions.mDebugCycles = True;
+    lAutoF
+    lAutoF.train(df1 , b1.mTimeVar , b1.mSignalVar, H);
+    lAutoF.getModelInfo();
+    # lAutoF.standrdPlots(name = "my_cycle_" + str(n));
+    lAutoF.mSignalDecomposition.mBestTransformation.mTimeInfo.mResolution
+    lCodeGenerator = tscodegen.cDecompositionCodeGenObject();
+    lSQL = lCodeGenerator.testGeneration(lAutoF);
