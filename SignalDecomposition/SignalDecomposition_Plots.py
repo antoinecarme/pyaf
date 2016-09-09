008da83691799a09b896098508cb5fbfdd8f0dd0
Antoine Carme <antoine.carme@laposte.net>
2016-09-09 13:20:35 +0200

    Allow plots in the REST API.

--- SignalDecomposition/SignalDecomposition_Plots.py
+++ SignalDecomposition/SignalDecomposition_Plots.py
@@ -25,6 +25,30 @@ def decomp_plot(df, time, signal, estimator, residue, name = None, max_length = 
     if(name is not None):
         fig.savefig(name + '_decomp_output.png')
 
+def decomp_plot_as_png_base64(df, time, signal, estimator, residue, name = None, max_length = 1000) :
+    assert(df.shape[0] > 0)
+    assert(df.shape[1] > 0)
+    assert(time in df.columns)
+    assert(signal in df.columns)
+    assert(estimator in df.columns)
+    assert(residue in df.columns)
+
+    df1 = df.tail(max_length);
+    fig, axs = plt.subplots(ncols=2, figsize=(16, 8))
+    df1.plot.line(time, [signal, estimator, residue], ax=axs[0] , grid = True)
+    residues =  df1[residue].values
+    lErrorStdDev = np.std(residues)
+    scistats.probplot(residues, dist="norm", plot=axs[1])
+
+    from io import BytesIO
+    figfile = BytesIO()
+    fig.savefig(figfile, format='png')
+    figfile.seek(0)  # rewind to beginning of file
+    import base64
+    figdata_png = base64.b64encode(figfile.getvalue())
+    return figdata_png.decode('utf8')
+    
+
 def prediction_interval_plot(df, time, signal, estimator, lower, upper, name = None, max_length = 1000) :
     assert(df.shape[0] > 0)
     assert(df.shape[1] > 0)
@@ -40,12 +64,40 @@ def prediction_interval_plot(df, time, signal, estimator, lower, upper, name = N
     lMax = np.mean(df1[signal]) +  np.std(df1[signal]) * 3;
     df1[lower] = df1[lower].apply(lambda x : x if (np.isnan(x) or x >= lMin) else np.nan);
     df1[upper] = df1[upper].apply(lambda x : x if (np.isnan(x) or x <= lMax) else np.nan);
-    fig, axs = plt.subplots(ncols=2, figsize=(32, 16))
-    df1.plot.line(time, [signal, estimator, lower, upper], ax=axs[0], grid = True)
-    axs[0].fill_between(df1[time], df1[lower], df1[upper], color='blue', alpha=.5)
+    fig, axs = plt.subplots(ncols=1, figsize=(16, 8))
+    df1.plot.line(time, [signal, estimator, lower, upper], ax=axs, grid = True)
+    axs.fill_between(df1[time], df1[lower], df1[upper], color='blue', alpha=.5)
 
     if(name is not None):
         fig.savefig(name + '_prediction_intervals_output.png')
+
+
+def prediction_interval_plot_as_png_base64(df, time, signal, estimator, lower, upper, name = None, max_length = 1000) :
+    assert(df.shape[0] > 0)
+    assert(df.shape[1] > 0)
+    assert(time in df.columns)
+    assert(signal in df.columns)
+    assert(estimator in df.columns)
+    assert(lower in df.columns)
+    assert(upper in df.columns)
+
+
+    df1 = df.tail(max_length).copy();
+    lMin = np.mean(df1[signal]) -  np.std(df1[signal]) * 3;
+    lMax = np.mean(df1[signal]) +  np.std(df1[signal]) * 3;
+    df1[lower] = df1[lower].apply(lambda x : x if (np.isnan(x) or x >= lMin) else np.nan);
+    df1[upper] = df1[upper].apply(lambda x : x if (np.isnan(x) or x <= lMax) else np.nan);
+    fig, axs = plt.subplots(ncols=1, figsize=(16, 8))
+    df1.plot.line(time, [signal, estimator, lower, upper], ax=axs, grid = True)
+    axs.fill_between(df1[time], df1[lower], df1[upper], color='blue', alpha=.5)
+
+    from io import BytesIO
+    figfile = BytesIO()
+    fig.savefig(figfile, format='png')
+    figfile.seek(0)  # rewind to beginning of file
+    import base64
+    figdata_png = base64.b64encode(figfile.getvalue())
+    return figdata_png.decode('utf8')
 
 
 def qqplot_residues(df , residue):
