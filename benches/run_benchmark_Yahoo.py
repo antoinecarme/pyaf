e010f6ee56ece1f1f6ca177930a656ce3a11eb1b
Antoine Carme <antoine.carme@laposte.net>
2016-09-29 12:51:03 +0200

    benchmarking scripts.

---
+++ benches/run_benchmark_Yahoo.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import Bench.TS_datasets as tsds
+import Bench.YahooStocks as ys
+
+symbol_lists = tsds.get_yahoo_symbol_lists();
+for k in symbol_lists.keys():
+    tester = ys.cYahoo_Tester(tsds.load_yahoo_stock_prices(k) , "YAHOO_STOCKS_" + k);
+    tester.run_multiprocessed(10);
