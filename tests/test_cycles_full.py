e964b02ba390e014862db333d2a7c9991d4c8571
Antoine Carme <antoine.carme@laposte.net>
2016-09-29 12:03:57 +0200

    Additonal tests.

---
+++ tests/test_cycles_full.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import AutoForecast as autof
+import Bench.TS_datasets as tsds
+
+#get_ipython().magic('matplotlib inline')
+
+
+lValues = [ k for k in range(2,24)];
+lValues = lValues + [ k for k in range(24, 128, 8)];
+for cyc in lValues:
+    print("TEST_CYCLES_START", cyc)
+    b1 = tsds.generate_random_TS(N = 320 , FREQ = 'D', seed = 0, trendtype = "constant", cycle_length = cyc, transform = "", sigma = 0.0);
+    df = b1.mPastData
+
+    # df.tail(10)
+    # df[:-10].tail()
+    # df[:-10:-1]
+    # df.describe()
+
+    lAutoF = autof.cForecastEngine()
+    lAutoF.mOptions.mCycleLengths = [ k for k in range(2,128) ];
+    lAutoF
+
+    H = b1.mHorizon;
+    lAutoF.train(df , b1.mTimeVar , b1.mSignalVar, H);
+    lAutoF.getModelInfo();
+
+    lAutoF.mSignalDecomposition.mBestTransformation.mTimeInfo.mResolution
+
+    dfapp_in = df.copy();
+    dfapp_in.tail()
+
+    # H = 12
+    dfapp_out = lAutoF.forecast(dfapp_in, H);
+    dfapp_out.tail(2 * H)
+    print("Forecast Columns " , dfapp_out.columns);
+    Forecast_DF = dfapp_out[[b1.mTimeVar , b1.mSignalVar, b1.mSignalVar + '_BestModelForecast']]
+    print(Forecast_DF.info())
+    print("Forecasts\n" , Forecast_DF.tail(H).values);
+
+    print("\n\n<ModelInfo>")
+    print(lAutoF.to_json());
+    print("</ModelInfo>\n\n")
+    print("\n\n<Forecast>")
+    print(Forecast_DF.to_json(date_format='iso'))
+    print("</Forecast>\n\n")
+    
+    print("TEST_CYCLES_END", cyc)
---
+++ tests/test_ozone_code_gen_2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import AutoForecast as autof
+import Bench.TS_datasets as tsds
+import warnings
+
+from CodeGen import TS_CodeGenerator as tscodegen
+
+#get_ipython().magic('matplotlib inline')
+
+
+with warnings.catch_warnings():
+    # warnings.simplefilter("error")
+
+    b1 = tsds.load_ozone()
+    df = b1.mPastData
+    
+    H = b1.mHorizon;
+    
+    N = df.shape[0];
+    for n in range(N-H , N-H+1, 2):
+        df1 = df.head(n).copy();
+        lAutoF = autof.cForecastEngine()
+        lAutoF
+        lAutoF.mOptions.mEnableSeasonals = False;
+        lAutoF.mOptions.mEnableCycles = False;
+        lAutoF.mOptions.mEnableARModels = False;
+        #    lAutoF.mOptions.mDebugCycles = True;
+        lAutoF.train(df1 , b1.mTimeVar , b1.mSignalVar, H);
+        lAutoF.getModelInfo();
+        lAutoF.mSignalDecomposition.mBestTransformation.mTimeInfo.mResolution
+        lCodeGenerator = tscodegen.cTimeSeriesCodeGenerator();
+        lSQL = lCodeGenerator.testGeneration(lAutoF);
