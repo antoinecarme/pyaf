0aaecabd449c6d718d988e470296e409b1849b5a
Antoine Carme <antoine.carme@laposte.net>
2016-10-01 10:51:59 +0200

    Updated these scripts.

--- tests/test_ozone_exogenous_code_gen.py
+++ tests/test_ozone_exogenous_code_gen.py
@@ -29,7 +29,8 @@ with warnings.catch_warnings():
         #    lEngine.mOptions.mEnableCycles = False;
         #    lEngine.mOptions.mEnableARModels = False;
         #    lEngine.mOptions.mDebugCycles = True;
-        lEngine.train(df1 , b1.mTimeVar , b1.mSignalVar, H, b1.mExogenousVariables);
+        lExogenousData = (b1.mExogenousDataFrame , b1.mExogenousVariables) 
+        lEngine.train(df1 , b1.mTimeVar , b1.mSignalVar, H, lExogenousData);
         lEngine.getModelInfo();
         lEngine.mSignalDecomposition.mBestTransformation.mTimeInfo.mResolution
         lEngine.standrdPlots(name = "outputs/my_arx_ozone_")
