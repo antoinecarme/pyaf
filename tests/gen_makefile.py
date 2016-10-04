3def15ade2150a9c24ff2e011fbf5deb17477550
Antoine Carme <antoine.carme@laposte.net>
2016-10-04 21:28:40 +0200

    1. reorganized all tests in sub-directories. 2. Added time resolution tests.

--- tests/gen_makefile.py
+++ tests/gen_makefile.py
@@ -1,16 +1,22 @@
 import os
 import glob
 
-test_target = "";
-for filename in glob.glob('tests/*.py'):
-    bn = os.path.basename(filename);
-    logfile = "logs/" + bn.replace(".py" , ".log");
-    print("#PROCESSING FILE : " , filename, bn);
-    
-    print(bn , " : " , "\n\t", "-ipython3" , filename , " > " , logfile , " 2>&1");
-    test_target = bn + " " + test_target;
+str1 = "bench codegen exog func perf real-life time_res";
+subdirs = str1.split();
 
-print("\n\ntests: ", test_target, "\n" , "\n");
+for subdir1 in subdirs:
+    test_target = "";
+    for filename in glob.glob("tests/" + subdir1 + "/*.py"):
+        bn = subdir1 + "/" + os.path.basename(filename);
+        logfile = bn.replace("/" , "_");
+        logfile = "logs/" + logfile.replace(".py" , ".log");
+        print("#PROCESSING FILE : " , filename, bn , logfile);
+        
+        print(bn , " : " , "\n\t", "-ipython3" , filename , " > " , logfile , " 2>&1");
+        test_target = bn + " " + test_target;
+
+    print("\n\n", subdir1, ": ", test_target, "\n" , "\n");
 
 print("\n# ********************************************** \n");
 
+print("all: " , str1 , "\n\t\n");
