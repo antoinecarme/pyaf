f6e528bc43596c5cfe1c918466d8998a061f2b81
Antoine Carme <antoine.carme@laposte.net>
2016-10-05 21:20:44 +0200

    Hierarchical forecasting prototype.

--- tests/gen_makefile.py
+++ tests/gen_makefile.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-str1 = "bench codegen exog func perf real-life time_res";
+str1 = "bench codegen exog func perf real-life time_res hierarchical";
 subdirs = str1.split();
 
 for subdir1 in subdirs:
