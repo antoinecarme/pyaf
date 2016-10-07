334500d8cbdae7e7d27aa46ca68e0cbe3b5bfb4a
Antoine Carme <antoine.carme@laposte.net>
2016-10-07 23:20:44 +0200

    Use standardization for continuous exogenous.

--- TS/Exogenous.py
+++ TS/Exogenous.py
@@ -8,15 +8,15 @@ class cExogenousInfo:
 
     def __init__(self):
         self.mExogenousVariables = None;
-        self.mExogenousDummies = None;
+        self.mEncodedExogenous = None;
         self.mExogenousVariableCategories = None;
         self.mExogenousDataFrame = None;
         self.mExogenousData = None;        
         self.mDateVariable = None;
+        self.mContExogenousStats = None;
         
     def info(self):
         lStr2 = "ExogenousVariables = '" + self.mExogD +"'";
-        lStr2 += " TimeMin=" + str(self.mTimeMin) +"";
         return lStr2;
 
 
@@ -27,15 +27,16 @@ class cExogenousInfo:
     def fit(self):
         self.mExogenousDataFrame = self.mExogenousData[0];
         self.mExogenousVariables = self.mExogenousData[1];
+        self.mDateVariable = self.mTimeInfo.mTime;
         # print("preProcessExogenousVariables , columns", self.mExogenousVariables);
         self.updateExogenousVariableInfo();
-        self.createExogenousDummies();
-        # print("preProcessExogenousVariables , dummy columns", self.mExogenousDummies);
+        self.createEncodedExogenous();
+        # print("preProcessExogenousVariables , dummy columns", self.mEncodedExogenous);
 
     def addVars(self, df):
         lExogDate = "exog_date_" + self.mDateVariable;
-        # print("EXOG_COLUMNS", self.mExogenousDummiesDataFrame.columns);
-        df = df.merge(self.mExogenousDummiesDataFrame,
+        # print("EXOG_COLUMNS", self.mEncodedExogenousDataFrame.columns);
+        df = df.merge(self.mEncodedExogenousDataFrame,
                       how='left',
                       left_on=self.mDateVariable,
                       right_on=lExogDate);
@@ -48,7 +49,7 @@ class cExogenousInfo:
         # print("AFTER_EXOG_TRANSFORM_DATASET" , df.shape, df.columns);
         return df;
 
-    def updateFutureExogenousDummies(self, df):
+    def updateFutureEncodedExogenous(self, df):
         for exog in self.mExogenousVariables:
             lList = self.mExogenousVariableCategories[exog];
             for lCat in lList:
@@ -58,27 +59,38 @@ class cExogenousInfo:
         return df;
 
         
-    def createExogenousDummies(self):
+    def createEncodedExogenous(self):
         self.mExogDummiesDataFrame = pd.DataFrame();
-        self.mExogenousDummies = [];
-        self.mExogenousDummiesDataFrame = pd.DataFrame();
-        self.mExogenousDummiesDataFrame["exog_date_" + self.mDateVariable] = self.mExogenousDataFrame[self.mDateVariable];
+        self.mEncodedExogenous = [];
+        self.mEncodedExogenousDataFrame = pd.DataFrame();
+        self.mEncodedExogenousDataFrame["exog_date_" + self.mDateVariable] = self.mExogenousDataFrame[self.mDateVariable];
         for exog in self.mExogenousVariables:
             lList = self.mExogenousVariableCategories[exog];
-            for lCat in lList:
-                lDummyName = "exog_dummy_" + exog + "=" + str(lCat);
-                self.mExogenousDummiesDataFrame[lDummyName] = self.mExogenousDataFrame[exog].apply(lambda x : 1 if (x == lCat) else 0);
-                self.mExogenousDummies = self.mExogenousDummies + [lDummyName];
+            if(lList is not None):
+                for lCat in lList:
+                    lDummyName = "exog_dummy_" + exog + "=" + str(lCat);
+                    self.mEncodedExogenousDataFrame[lDummyName] = self.mExogenousDataFrame[exog].apply(lambda x : 1 if (x == lCat) else 0);
+                    self.mEncodedExogenous = self.mEncodedExogenous + [lDummyName];
+            else:
+                lExogStats = self.mContExogenousStats[exog];
+                self.mEncodedExogenousDataFrame[exog] = (self.mExogenousDataFrame[exog] - lExogStats[0])/ lExogStats[1];
+                self.mEncodedExogenousDataFrame[exog].fillna(0.0, inplace=True);
+                self.mEncodedExogenous = self.mEncodedExogenous + [exog];
 
 
     def updateExogenousVariableInfo(self):
         self.mExogenousVariableCategories = {};
+        self.mContExogenousStats = {};
+        lEstimFrame = self.mTimeInfo.getEstimPart(self.mExogenousDataFrame)
         for exog in self.mExogenousVariables:
-            # use nan as a category
-            lVC = self.mExogenousDataFrame[exog].value_counts(dropna = False);
-            # TODO : 5 categories. to be added in options.
-            NCat = 5;
-            lList = lVC.index[0:NCat].tolist();
-            print("most_frequent_categories_for" , exog, lList);
-            self.mExogenousVariableCategories[exog] = lList;
-
+            if(self.mExogenousDataFrame[exog].dtype == np.object):
+                # use nan as a category
+                lVC = lEstimFrame[exog].value_counts(dropna = False);
+                # TODO : 5 categories. to be added in options.
+                NCat = 5;
+                lList = lVC.index[0:NCat].tolist();
+                print("most_frequent_categories_for" , exog, lList);
+                self.mExogenousVariableCategories[exog] = lList;
+            else:
+                self.mExogenousVariableCategories[exog] = None;
+                self.mContExogenousStats[exog] = (lEstimFrame[exog].mean(), lEstimFrame[exog].std());
