c3fe288c234e27665dd9fee171af0783a6c550b2
Antoine Carme <antoine.carme@laposte.net>
2016-10-08 12:53:19 +0200

    Added +        self.mMaxExogenousCategories = 5;

--- TS/Options.py
+++ TS/Options.py
@@ -11,6 +11,7 @@ class cSignalDecomposition_Options:
         self.mTimeDeltaComputationMethod = "AVG"; # can be "AVG", "MODE", "USER"
         self.mUserTimeDelta = None;
         self.mBusinessDaysOnly = False;
+        self.mMaxExogenousCategories = 5;
         self.mNoCoxBoxOrders = [];
         self.mCoxBoxOrders = [-2.0, -1.0 , 0.0,  2.0];
         self.mExtensiveCoxBoxOrders = [-2, -1, -0.5, -0.33 , -0.25 , 0.0, 2, 0.5, 0.33 , 0.25];
