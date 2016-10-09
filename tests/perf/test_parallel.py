3c73b5d13dfc7aab96e87ec13fad0fa7a7f2928c
Antoine Carme <antoine.carme@laposte.net>
2016-10-09 19:06:00 +0200

    soem basic test to actibvate muiltiprocessing.

---
+++ tests/perf/test_parallel.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+
+import sys,os
+# for timing
+import time
+
+import multiprocessing as mp
+import threading
+from multiprocessing.dummy import Pool as ThreadPool
+
+import warnings
+
+def createDirIfNeeded(dirname):
+    try:
+        os.mkdir(dirname);
+    except:
+        pass
+
+class cBenchmarkError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg);
+        pass
+    
+    
+def run_bench_process(a):
+    try:
+        print("RUNNING" , a.mName);
+        a.mResult = a.mValues.mean();
+        print(a.mResult);
+        return a;
+    except cBenchmarkError as error:
+        print("BENCHMARKING_ERROR '" + a.mName + "'");
+        logger.error(error)
+        pass;
+    except:
+        print("BENCHMARK_FAILURE '" + a.mName + "'");
+        raise
+
+class cGeneric_Tester_Arg:
+    def __init__(self , name, values):
+        self.mName = name;
+        self.mValues = values;
+        self.mResult = None;
+
+def run_multiprocessed(nbprocesses = 20):
+    pool = mp.Pool(nbprocesses)
+    args = []
+    for sig in range(100):
+        values = pd.Series();
+        values = np.arange(1000000);
+        a = cGeneric_Tester_Arg("process_" + str(sig), values);
+        args = args + [a];
+        
+    out = {};
+    for res in pool.imap(run_bench_process, args):
+        out[res.mName] = res;
+    return out;
+
+def run():
+    results = run_multiprocessed(nbprocesses = 12);
+    for res in results.keys():
+        print(res , results[res].mResult);
+
+
+with warnings.catch_warnings():
+    warnings.simplefilter("error")
+    run();
