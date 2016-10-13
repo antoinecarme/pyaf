65e27473cd2396c2d498d91a53d1c87f8d8562fc
Antoine Carme <antoine.carme@laposte.net>
2016-10-13 14:05:35 +0200

    Added copyright message.

--- Bench/GenericBenchmark.py
+++ Bench/GenericBenchmark.py
@@ -1,9 +1,15 @@
+# Copyright (C) 2016 Antoine Carme <Antoine.Carme@Laposte.net>
+# All rights reserved.
+
+# This file is part of the Python Automatic Forecasting (PyAF) library and is made available under
+# the terms of the 3 Clause BSD license
+
 import pandas as pd
 import numpy as np
 
-import AutoForecast.ForecastEngine as autof
+import pyaf.ForecastEngine as autof
 from AutoForecast.CodeGen import TS_CodeGenerator as tscodegen
-import AutoForecast.Bench.TS_datasets as tsds
+import pyaf.Bench.TS_datasets as tsds
 
 import sys,os
 # for timing
