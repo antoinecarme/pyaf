98a3733a30ad378b2589081328226585110da234
Antoine Carme <antoine.carme@laposte.net>
2016-10-13 14:06:02 +0200

    Added copyright message.

--- WS/WS_Backend.py
+++ WS/WS_Backend.py
@@ -1,9 +1,15 @@
+# Copyright (C) 2016 Antoine Carme <Antoine.Carme@Laposte.net>
+# All rights reserved.
+
+# This file is part of the Python Automatic Forecasting (PyAF) library and is made available under
+# the terms of the 3 Clause BSD license
+
 import pandas as pd
 import numpy as np
 import datetime as dt
 
-import AutoForecast.ForecastEngine as autof
-import AutoForecast.Bench.TS_datasets as tsds
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
 
 from flask import Flask, jsonify, request
 import logging
