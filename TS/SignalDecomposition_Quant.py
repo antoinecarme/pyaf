9aec197bf2a6f0faf6d5b4ccfb7abae3c7943a89
Antoine Carme <antoine.carme@laposte.net>
2016-10-13 14:04:19 +0200

    Added copyright message.

--- TS/SignalDecomposition_Quant.py
+++ TS/SignalDecomposition_Quant.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2016 Antoine Carme <Antoine.Carme@Laposte.net>
+# All rights reserved.
+
+# This file is part of the Python Automatic Forecasting (PyAF) library and is made available under
+# the terms of the 3 Clause BSD license
+
 import pandas as pd
 import numpy as np
 import datetime
--- TS/SignalHierarchy.py
+++ TS/SignalHierarchy.py
@@ -1,8 +1,14 @@
+# Copyright (C) 2016 Antoine Carme <Antoine.Carme@Laposte.net>
+# All rights reserved.
+
+# This file is part of the Python Automatic Forecasting (PyAF) library and is made available under
+# the terms of the 3 Clause BSD license
+
 import pandas as pd
 import numpy as np
 
 
-import AutoForecast.ForecastEngine as autof
+import pyaf.ForecastEngine as autof
 from . import Options as tsopts
 from . import Perf as tsperf
 from . import Utils as tsutil
