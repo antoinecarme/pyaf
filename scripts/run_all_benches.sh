995c7c057d3ab6e04d0c67357af402958f34ee6b
Antoine Carme <antoine.carme@laposte.net>
2016-10-13 23:55:01 +0200

    Updated these scripts.

--- scripts/run_all_benches.sh
+++ scripts/run_all_benches.sh
@@ -1,8 +1,15 @@
 mkdir logs
-ipython3 test_M1Comp.py &> logs/log.m1 &
-ipython3 test_M2Comp.py &> logs/log.m2 &
-ipython3 test_M3Comp.py &> logs/log.m3 &
-ipython3 test_M4Comp.py &> logs/log.m4 &
-ipython3 test_NN3.py &> logs/log.nn3
-ipython3 test_NN5.py &> logs/log.nn5
-
+# this file is a utility that is used to run the benchamrks .... takes too much time to run. 
+# The output of the benchmarks will be in logs directory.
+# This script is used for bernchmarks , not for tests.
+# tests are located in the 'tests' directory. There is a tests/Makefile.
+ipython3 benches/run_benchmark_artificial.py
+ipython3 benches/run_benchmark_M1Comp.py
+ipython3 benches/run_benchmark_M2Comp.py
+ipython3 benches/run_benchmark_M3Comp.py
+ipython3 benches/run_benchmark_M4Comp.py
+ipython3 benches/run_benchmark_MComp.py
+ipython3 benches/run_benchmark_MWH.py
+ipython3 benches/run_benchmark_NN3.py
+ipython3 benches/run_benchmark_NN5.py
+ipython3 benches/run_benchmark_Yahoo.py
--- setup_python_path.sh
+++ setup_python_path.sh
@@ -1,1 +1,5 @@
-export PYTHONPATH=$PYTHONPATH:./src:./Bench
+# This script must be run from the pyaf directory to setup the path to the pyaf modules.
+# the goal is only to add the 'pyaf' parent directory in the PYTHONPATH envioronemnt variable.
+export PYTHONPATH=$PYTHONPATH:`pwd`/..
+
+
