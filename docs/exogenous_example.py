8731c42cb0735ffcbcaa9c7f7619ac0423ced962
Antoine Carme <antoine.carme@laposte.net>
2016-11-09 23:01:46 +0100

    Example used in the exogenous notebook.

---
+++ docs/exogenous_example.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import datetime
+
+csvfile_link = "data/ozone-la-exogenous-2.csv"
+exog_dataframe = pd.read_csv(csvfile_link);
+exog_dataframe['Date'] = exog_dataframe['Date'].astype(np.datetime64);
+# print(exog_dataframe.info())
+exog_dataframe.head()
+
+import pyaf.ForecastEngine as autof
+lEngine = autof.cForecastEngine()
+
+csvfile_link = "https://raw.githubusercontent.com/antoinecarme/TimeSeriesData/master/ozone-la.csv"
+ozone_dataframe = pd.read_csv(csvfile_link);
+ozone_dataframe['Date'] = ozone_dataframe['Month'].apply(lambda x : datetime.datetime.strptime(x, "%Y-%m"))
+
+# print(ozone_dataframe.info())
+
+lExogenousData = (exog_dataframe , ['Exog2' , 'Exog3' , 'Exog4',  'Exog5']) 
+
+lEngine.train(ozone_dataframe , 'Date' , 'Ozone', 12 , lExogenousData);
+
+forecast_dataframe = lEngine.forecast(ozone_dataframe, 12)
