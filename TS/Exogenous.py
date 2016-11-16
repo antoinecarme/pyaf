c207a7e3afc7cb8dbb7a3154f7d1ece9748b8b73
Antoine Carme <antoine.carme@laposte.net>
2016-11-16 19:57:15 +0100

    Added some exclusion mechanism to filter out low-quality predictors.

--- TS/Exogenous.py
+++ TS/Exogenous.py
@@ -22,6 +22,7 @@ class cExogenousInfo:
         self.mExogenousData = None;        
         self.mDateVariable = None;
         self.mContExogenousStats = None;
+        self.mExcluded = [];
         
     def info(self):
         lStr2 = "ExogenousVariables = '" + self.mExogD +"'";
@@ -78,31 +79,50 @@ class cExogenousInfo:
         self.mEncodedExogenousDataFrame = pd.DataFrame();
         self.mEncodedExogenousDataFrame[self.mDateVariable] = self.mExogenousDataFrame[self.mDateVariable];
         for exog in self.mExogenousVariables:
-            lList = self.mExogenousVariableCategories[exog];
-            if(lList is not None):
-                for lCat in lList:
-                    lDummyName = exog + "=" + str(lCat);
-                    self.mEncodedExogenousDataFrame[lDummyName] = np.where(self.mExogenousDataFrame[exog] == lCat , 1, 0);
-                    self.mEncodedExogenous = self.mEncodedExogenous + [lDummyName];
+            if(exog not in self.mExcluded):
+                lList = self.mExogenousVariableCategories[exog];
+                if(lList is not None):
+                    for lCat in lList:
+                        lDummyName = exog + "=" + str(lCat);
+                        lVec = np.where(self.mExogenousDataFrame[exog] == lCat , 1, 0);
+                        self.mEncodedExogenousDataFrame[lDummyName] = lVec;
+                        self.mEncodedExogenous = self.mEncodedExogenous + [lDummyName];
+                else:
+                    lExogStats = self.mContExogenousStats[exog];
+                    self.mEncodedExogenousDataFrame[exog] = (self.mExogenousDataFrame[exog] - lExogStats[0])/ lExogStats[1];
+                    self.mEncodedExogenousDataFrame[exog].fillna(0.0, inplace=True);
+                    self.mEncodedExogenous = self.mEncodedExogenous + [exog];
             else:
-                lExogStats = self.mContExogenousStats[exog];
-                self.mEncodedExogenousDataFrame[exog] = (self.mExogenousDataFrame[exog] - lExogStats[0])/ lExogStats[1];
-                self.mEncodedExogenousDataFrame[exog].fillna(0.0, inplace=True);
-                self.mEncodedExogenous = self.mEncodedExogenous + [exog];
+                print("EXCLUDED" , exog);
 
 
     def updateExogenousVariableInfo(self):
         self.mExogenousVariableCategories = {};
         self.mContExogenousStats = {};
-        lEstimFrame = self.mTimeInfo.getEstimPart(self.mExogenousDataFrame)
+        # Compute these stats only on the estimation part.
+        lEstimFrame = self.mExogenousDataFrame[self.mExogenousDataFrame[self.mDateVariable] >= self.mTimeInfo.mTimeMin]
+        lEstimFrame = lEstimFrame[lEstimFrame[self.mDateVariable] <= self.mTimeInfo.mTimeMax]
         for exog in self.mExogenousVariables:
             if(self.mExogenousDataFrame[exog].dtype == np.object):
                 # use nan as a category
                 lVC = lEstimFrame[exog].value_counts(dropna = False);
                 NCat = self.mOptions.mMaxExogenousCategories;
-                lList = lVC.index[0:NCat].tolist();
+                NCat = min(NCat , lVC.shape[0]);
+                # print("EXOGENOUS_DATA", lVC.head(NCat));
+                lList = [];
+                for i in range(NCat):
+                    if(lVC[i] > 5):
+                        lList.append(lVC.index[i]);
+                # lListlVC.index[0:NCat].tolist();
                 # print("most_frequent_categories_for" , exog, lList);
-                self.mExogenousVariableCategories[exog] = lList;
+                if(len(lList) == 0):
+                    self.mExcluded.append(exog);
+                else:
+                    self.mExogenousVariableCategories[exog] = lList;
             else:
                 self.mExogenousVariableCategories[exog] = None;
-                self.mContExogenousStats[exog] = (lEstimFrame[exog].mean(), lEstimFrame[exog].std());
+                stat_exog = (lEstimFrame[exog].mean(), lEstimFrame[exog].std());
+                if(stat_exog[1] > 1e-5):
+                    self.mContExogenousStats[exog] = stat_exog;
+                else:
+                    self.mExcluded.append(exog);
