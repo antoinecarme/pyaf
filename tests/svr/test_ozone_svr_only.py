4284ffd90bb925c08045a2cf1fd662645fa10492
Antoine Carme <antoine.carme@laposte.net>
2016-12-22 09:27:21 +0100

    Add Support Vector Regression Models #16
    Added some tests.

---
+++ tests/svr/test_ozone_svr_only.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
+
+import logging
+import logging.config
+
+#logging.config.fileConfig('logging.conf')
+
+logging.basicConfig(level=logging.INFO)
+
+#get_ipython().magic('matplotlib inline')
+
+b1 = tsds.load_ozone()
+df = b1.mPastData
+
+df.head()
+
+
+lEngine = autof.cForecastEngine()
+lEngine
+
+H = b1.mHorizon;
+# lEngine.mOptions.enable_slow_mode();
+lEngine.mOptions.mDebugPerformance = True;
+lEngine.mOptions.mParallelMode = True;
+lEngine.mOptions.mEnableTimeBasedTrends = False;
+lEngine.mOptions.mEnableStochasticTrends = False;
+lEngine.mOptions.mEnableARModels = False;
+lEngine.mOptions.mEnableARXModels = False;
+lEngine.mOptions.mEnableRNNModels = False;
+lEngine.mOptions.mEnableSVRModels = True;
+lEngine.mOptions.disable_all_transformations();
+lEngine.mOptions.mActiveTransformation["None"] = True;
+
+lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
+lEngine.getModelInfo();
+print(lEngine.mSignalDecomposition.mTrPerfDetails.head());
+
+lEngine.mSignalDecomposition.mBestModel.mTimeInfo.mResolution
+
+lEngine.standrdPlots("outputs/my_ozone_svr_only");
+
+dfapp_in = df.copy();
+dfapp_in.tail()
+
+#H = 12
+dfapp_out = lEngine.forecast(dfapp_in, H);
+dfapp_out.to_csv("outputs/ozone_apply_out.csv")
+dfapp_out.tail(2 * H)
+print("Forecast Columns " , dfapp_out.columns);
+Forecast_DF = dfapp_out[[b1.mTimeVar , b1.mSignalVar, b1.mSignalVar + '_Forecast']]
+print(Forecast_DF.info())
+print("Forecasts\n" , Forecast_DF.tail(H));
+
+print("\n\n<ModelInfo>")
+print(lEngine.to_json());
+print("</ModelInfo>\n\n")
+print("\n\n<Forecast>")
+print(Forecast_DF.to_json(date_format='iso'))
+print("</Forecast>\n\n")
+
