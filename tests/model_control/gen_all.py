0cb0743cc40df718c01d03bab3df2f75e6400d2a
Antoine Carme <antoine.carme@laposte.net>
2016-12-23 10:07:02 +0100

    Add the possibility to disable or force a model #17
    Some tests.

---
+++ tests/model_control/gen_all.py
@@ -0,0 +1,41 @@
+import os
+
+def createDirIfNeeded(dirname):
+    try:
+        os.mkdir(dirname);
+    except:
+        pass
+
+
+
+
+lKnownTransformations = ['None', 'Difference', 'RelativeDifference',
+                         'Integration', 'BoxCox',
+                         'Quantization', 'Logit',
+                         'Fisher', 'Anscombe'];
+lKnownTrends = ['ConstantTrend', 
+                'Lag1Trend', 'LinearTrend', 'PolyTrend', 
+                'MovingAverage', 'MovingMedian'];
+lKnownPeriodics = ['NoCycle', 'BestCycle',
+                   'Seasonal_MonthOfYear' ,
+                   'Seasonal_Second' ,
+                   'Seasonal_Minute' ,
+                   'Seasonal_Hour' ,
+                   'Seasonal_DayOfWeek' ,
+                   'Seasonal_DayOfMonth',
+                   'Seasonal_WeekOfYear'];
+lKnownAutoRegressions = ['NoAR' , 'AR' , 'ARX' , 'SVR' , 'MLP' , 'LSTM'];
+
+createDirIfNeeded("tests/model_control/detailed/");
+
+for transf in lKnownTransformations:
+    createDirIfNeeded("tests/model_control/detailed/transf_" + transf);
+    for trend in lKnownTrends:
+        for per in lKnownPeriodics:
+            for autoreg in lKnownAutoRegressions:
+                filename= "tests/model_control/detailed/transf_" + str(transf) + "/model_control_one_enabled_" + str(transf) + "_" + str(trend) + "_" + str(per) + "_" + str(autoreg) + ".py";
+                file = open(filename, "w");
+                print("WRTITING_FILE" , filename);
+                file.write("import pyaf.tests.model_control.test_ozone_custom_models_enabled as testmod\n");
+                file.write("\n\ntestmod.build_model( ['" + str(transf) + "'] , ['" + str(trend) + "'] , ['" + str(per) + "'] , ['" + str(autoreg) + "'] );");
+                file.close();
---
+++ tests/model_control/gen_makefile.py
@@ -0,0 +1,26 @@
+import os
+import glob
+
+str1 = "model_control";
+subdirs = str1.split();
+
+
+for subdir1 in subdirs:
+    test_target = "";
+    scripts = glob.glob("tests/" + subdir1 + "*/*/*/*.py");
+    # scripts = scripts + glob.glob("tests/" + subdir1 + "*/*.py");
+                       
+    for filename in scripts:
+        bn = os.path.basename(filename);
+        logfile = bn.replace("/" , "_");
+        logfile = "logs/" + logfile.replace(".py" , ".log");
+        print("#PROCESSING FILE : " , filename, bn , logfile);
+        
+        print(bn , " : " , "\n\t", "-ipython3" , filename , " > " , logfile , " 2>&1");
+        test_target = bn + " " + test_target;
+
+    print("\n\n", subdir1, ": ", test_target, "\n" , "\n");
+
+print("\n# ********************************************** \n");
+
+print("all: " , str1 , "\n\t\n");
