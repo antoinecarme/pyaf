ef17704f54367b460dc8285f347f003150a345d7
Antoine Carme <antoine.carme@laposte.net>
2016-12-25 11:40:38 +0100

    Use Travis-CI #18
    Updated makefile.

--- tests/gen_makefile.py
+++ tests/gen_makefile.py
@@ -1,18 +1,27 @@
 import os
 import glob
 
-str1 = "bench codegen exog func perf real-life time_res hierarchical transformations";
+str1 = "artificial bugs exog hierarchical model_control perf svr transformations bench func neuralnet real-life  time_res";
 subdirs = str1.split();
+
+print("PYTHON=python3\n\n");
 
 for subdir1 in subdirs:
     test_target = "";
     for filename in glob.glob("tests/" + subdir1 + "/*.py"):
         bn = subdir1 + "/" + os.path.basename(filename);
         logfile = bn.replace("/" , "_");
-        logfile = "logs/" + logfile.replace(".py" , ".log");
-        print("#PROCESSING FILE : " , filename, bn , logfile);
+        logname = logfile.replace(".py" , ".log");
+        logfile = "logs/" + logname;
+        reflogfile = "tests/references/" + logname;
+        difffile = logfile + ".diff"
+        # print("#PROCESSING FILE : " , filename, bn , logfile);
         
-        print(bn , " : " , "\n\t", "-ipython3" , filename , " > " , logfile , " 2>&1");
+        print(bn , " : " , "\n\t", "-$(PYTHON) " , filename , " > " , logfile , " 2>&1");
+        print("\t", "-diff " , logfile , reflogfile , " > " , difffile, "\n");
+        print("\t", "cat " ,  difffile, "\n");
+        
+        
         test_target = bn + " " + test_target;
 
     print("\n\n", subdir1, ": ", test_target, "\n" , "\n");
