c6000760eb7c5bbd7a81d1b5fdf1cc80d052386f
Antoine Carme <antoine.carme@laposte.net>
2016-12-25 11:57:40 +0100

    Use Travis-CI #18
    Avoid unnecessary scripts used for the test framework.

--- tests/gen_makefile.py
+++ tests/gen_makefile.py
@@ -9,20 +9,22 @@ print("PYTHON=python3\n\n");
 for subdir1 in subdirs:
     test_target = "";
     for filename in glob.glob("tests/" + subdir1 + "/*.py"):
-        bn = subdir1 + "/" + os.path.basename(filename);
-        logfile = bn.replace("/" , "_");
-        logname = logfile.replace(".py" , ".log");
-        logfile = "logs/" + logname;
-        reflogfile = "tests/references/" + logname;
-        difffile = logfile + ".diff"
-        # print("#PROCESSING FILE : " , filename, bn , logfile);
+        lShortName = os.path.basename(filename);
+        if(not lShortName.lower().startswith("gen_all") and
+           not lShortName.lower().startswith("gen_makefile") ):
+            bn = subdir1 + "/" + lShortName;
+            logfile = bn.replace("/" , "_");
+            logname = logfile.replace(".py" , ".log");
+            logfile = "logs/" + logname;
+            reflogfile = "tests/references/" + logname;
+            difffile = logfile + ".diff"
+            # print("#PROCESSING FILE : " , filename, bn , logfile);
         
-        print(bn , " : " , "\n\t", "-$(PYTHON) " , filename , " > " , logfile , " 2>&1");
-        print("\t", "-diff " , logfile , reflogfile , " > " , difffile, "\n");
-        print("\t", "cat " ,  difffile, "\n");
-        
-        
-        test_target = bn + " " + test_target;
+            print(bn , " : " , "\n\t", "-$(PYTHON) " , filename , " > " , logfile , " 2>&1");
+            print("\t", "-diff " , logfile , reflogfile , " > " , difffile, "\n");
+            print("\t", "cat " ,  difffile, "\n");
+                
+            test_target = bn + " " + test_target;
 
     print("\n\n", subdir1, ": ", test_target, "\n" , "\n");
 
