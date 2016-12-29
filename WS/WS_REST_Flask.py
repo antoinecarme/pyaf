772d7f7b385ecb3d6d3e79a4d03c9c583f9cfc1b
CARME Antoine <Antoine.Carme@Laposte.net>
2016-12-29 12:31:42 +0100

    Update WS_REST_Flask.py

--- WS/WS_REST_Flask.py
+++ WS/WS_REST_Flask.py
@@ -111,5 +111,5 @@ def removeOneModel(name):
     return jsonify_models();
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
     
