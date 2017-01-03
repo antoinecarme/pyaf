5cc459c0bb990c8828c60ff5b1914c27bbc5e5b2
Antoine Carme <antoine.carme@laposte.net>
2017-01-03 15:54:41 +0100

    Enhance artificial dataset tests. #6
    Added sumsum transformation

---
+++ tests/artificial/transf_cumsum/trend_poly/cycle_30/ar_/test_artificial_128_cumsum_poly_30__100.py
@@ -0,0 +1,9 @@
+import Bench.TS_datasets as tsds
+import tests.artificial.process_artificial_dataset as art
+
+
+
+
+dataset = tsds.generate_random_TS(N = 128 , FREQ = 'D', seed = 0, trendtype = "poly", cycle_length = 30, transform = "cumsum", sigma = 0.0, exog_count = 100);
+
+art.process_dataset(dataset);
---
+++ tests/artificial/transf_cumsum/trend_poly/cycle_7/ar_/test_artificial_32_cumsum_poly_7__0.py
@@ -0,0 +1,9 @@
+import Bench.TS_datasets as tsds
+import tests.artificial.process_artificial_dataset as art
+
+
+
+
+dataset = tsds.generate_random_TS(N = 32 , FREQ = 'D', seed = 0, trendtype = "poly", cycle_length = 7, transform = "cumsum", sigma = 0.0, exog_count = 0);
+
+art.process_dataset(dataset);
