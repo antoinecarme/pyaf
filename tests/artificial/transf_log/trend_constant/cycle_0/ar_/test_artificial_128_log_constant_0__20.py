942a6d494482b7a9ed4cda74a38a39b20a11fa01
Antoine Carme <antoine.carme@laposte.net>
2017-01-03 15:57:42 +0100

    Enhance artificial dataset tests. #6
    Added log transformation

---
+++ tests/artificial/transf_log/trend_constant/cycle_0/ar_/test_artificial_128_log_constant_0__20.py
@@ -0,0 +1,9 @@
+import Bench.TS_datasets as tsds
+import tests.artificial.process_artificial_dataset as art
+
+
+
+
+dataset = tsds.generate_random_TS(N = 128 , FREQ = 'D', seed = 0, trendtype = "constant", cycle_length = 0, transform = "log", sigma = 0.0, exog_count = 20);
+
+art.process_dataset(dataset);
---
+++ tests/artificial/transf_log/trend_constant/cycle_5/ar_/test_artificial_1024_log_constant_5__100.py
@@ -0,0 +1,9 @@
+import Bench.TS_datasets as tsds
+import tests.artificial.process_artificial_dataset as art
+
+
+
+
+dataset = tsds.generate_random_TS(N = 1024 , FREQ = 'D', seed = 0, trendtype = "constant", cycle_length = 5, transform = "log", sigma = 0.0, exog_count = 100);
+
+art.process_dataset(dataset);
