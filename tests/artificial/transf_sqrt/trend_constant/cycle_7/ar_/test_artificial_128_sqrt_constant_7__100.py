384c8e26ddaec896811bc1f63e33b534258c37cf
Antoine Carme <antoine.carme@laposte.net>
2017-01-03 15:56:27 +0100

    Enhance artificial dataset tests. #6
    Added sqrt transformation

---
+++ tests/artificial/transf_sqrt/trend_constant/cycle_7/ar_/test_artificial_128_sqrt_constant_7__100.py
@@ -0,0 +1,9 @@
+import Bench.TS_datasets as tsds
+import tests.artificial.process_artificial_dataset as art
+
+
+
+
+dataset = tsds.generate_random_TS(N = 128 , FREQ = 'D', seed = 0, trendtype = "constant", cycle_length = 7, transform = "sqrt", sigma = 0.0, exog_count = 100);
+
+art.process_dataset(dataset);
---
+++ tests/artificial/transf_sqrt/trend_poly/cycle_7/ar_/test_artificial_32_sqrt_poly_7__100.py
@@ -0,0 +1,9 @@
+import Bench.TS_datasets as tsds
+import tests.artificial.process_artificial_dataset as art
+
+
+
+
+dataset = tsds.generate_random_TS(N = 32 , FREQ = 'D', seed = 0, trendtype = "poly", cycle_length = 7, transform = "sqrt", sigma = 0.0, exog_count = 100);
+
+art.process_dataset(dataset);
