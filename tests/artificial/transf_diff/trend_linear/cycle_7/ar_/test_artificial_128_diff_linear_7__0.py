666335aacd2852ceb72bd07c8676a1b1561c117c
Antoine Carme <antoine.carme@laposte.net>
2017-01-03 15:53:47 +0100

    Enhance artificial dataset tests. #6
    Added diff transformation

---
+++ tests/artificial/transf_diff/trend_linear/cycle_7/ar_/test_artificial_128_diff_linear_7__0.py
@@ -0,0 +1,9 @@
+import Bench.TS_datasets as tsds
+import tests.artificial.process_artificial_dataset as art
+
+
+
+
+dataset = tsds.generate_random_TS(N = 128 , FREQ = 'D', seed = 0, trendtype = "linear", cycle_length = 7, transform = "diff", sigma = 0.0, exog_count = 0);
+
+art.process_dataset(dataset);
