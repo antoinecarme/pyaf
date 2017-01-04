5875e5eef2eec6184cc5d807c94020f75e04f915
Antoine Carme <antoine.carme@laposte.net>
2017-01-04 12:53:14 +0100

    Artificial dataset test failure #21
    Simpler failing test.

---
+++ tests/bugs/issue_21/test_ozone_max_autoreg_5.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+
+import pandas as pd
+import numpy as np
+
+
+# import '.' as pyaf_new_name
+# pyaf=pyaf_new_name
+# from pyaf
+import ForecastEngine as autof
+import Bench.TS_datasets as tsds
+
+#get_ipython().magic('matplotlib inline')
+
+b1 = tsds.load_ozone()
+df = b1.mPastData
+
+#df.tail(10)
+#df[:-10].tail()
+#df[:-10:-1]
+#df.describe()
+
+
+lEngine = autof.cForecastEngine()
+lEngine
+
+H = b1.mHorizon;
+lEngine.mOptions.mMaxFeatureForAutoreg = 5;
+lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
+lEngine.getModelInfo();
+print(lEngine.mSignalDecomposition.mTrPerfDetails.head());
+
+lEngine.mSignalDecomposition.mBestModel.mTimeInfo.mResolution
+
+lEngine.standrdPlots("outputs/my_ozone_max_ar_5_");
+
+dfapp_in = df.copy();
+dfapp_in.tail()
+
+#H = 12
+dfapp_out = lEngine.forecast(dfapp_in, H);
+#dfapp_out.to_csv("outputs/ozone_apply_out.csv")
+dfapp_out.tail(2 * H)
+print("Forecast Columns " , dfapp_out.columns);
+Forecast_DF = dfapp_out[[b1.mTimeVar , b1.mSignalVar, b1.mSignalVar + '_Forecast']]
+print(Forecast_DF.info())
+print("Forecasts\n" , Forecast_DF.tail(H));
+
+print("\n\n<ModelInfo>")
+print(lEngine.to_json());
+print("</ModelInfo>\n\n")
+print("\n\n<Forecast>")
+print(Forecast_DF.to_json(date_format='iso'))
+print("</Forecast>\n\n")
+
