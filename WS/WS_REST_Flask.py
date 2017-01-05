73f3e2b38b3173b19bfeb325f7750a36f6489fbd
CARME Antoine <Antoine.Carme@Laposte.net>
2017-01-05 17:20:35 +0100

    Update WS_REST_Flask.py

--- WS/WS_REST_Flask.py
+++ WS/WS_REST_Flask.py
@@ -111,4 +111,5 @@ def removeOneModel(name):
     return jsonify_models();
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port, debug=True)
