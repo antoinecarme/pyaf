8d52e5670879569ca8eebbe98b9c77387df8a987
CARME Antoine <Antoine.Carme@Laposte.net>
2017-01-05 18:30:15 +0100

    Tweak Heroku

--- WS/WS_REST_Flask.py
+++ WS/WS_REST_Flask.py
@@ -112,5 +112,6 @@ def removeOneModel(name):
     return jsonify_models();
 
 if __name__ == '__main__':
+    print(os.environ);
     port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', port=port, debug=True)
