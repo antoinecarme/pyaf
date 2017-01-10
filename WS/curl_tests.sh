01595be82f30dbf047423741e563c2060bcff185
Antoine Carme <antoine.carme@laposte.net>
2017-01-10 14:09:59 +0100

    Added two models, one with and one wihtout exogenous data

--- WS/curl_tests.sh
+++ WS/curl_tests.sh
@@ -1,5 +1,5 @@
-PYAF_URL="http://pyaf.herokuapp.com/model"
-# PYAF_URL="http://0.0.0.0:8081/model"
+# PYAF_URL="http://pyaf.herokuapp.com/model"
+PYAF_URL="http://0.0.0.0:8081/model"
 CONTENT_TYPE="Content-Type: application/json"
 
 # create a model
@@ -29,5 +29,14 @@ curl --header "$CONTENT_TYPE" -X POST --data "$DATA3" "$PYAF_URL"
 DATA3="{\"SignalVar\":\"Close\", \"Horizon\":\"21\", \"CSVFile\": \"http://chart.finance.yahoo.com/table.csv?s=GOOG&a=8&b=14&c=2015&d=9&e=14&f=2016&g=d&ignore=.csv\"}";
 curl --header "$CONTENT_TYPE" -X POST --data "$DATA3" "$PYAF_URL"
 
-DATA3="{\"SignalVar\":\"Close\", \"Horizon\":\"21\", \"Present\":\"2016-01-01\", \"CSVFile\": \"http://chart.finance.yahoo.com/table.csv?s=GOOG&a=8&b=14&c=2015&d=9&e=14&f=2016&g=d&ignore=.csv\"}";
-curl --header "$CONTENT_TYPE" -X POST --data "$DATA3" "$PYAF_URL"
+DATA4="{\"SignalVar\":\"Close\", \"Horizon\":\"21\", \"Present\":\"2016-01-01\", \"CSVFile\": \"http://chart.finance.yahoo.com/table.csv?s=GOOG&a=8&b=14&c=2015&d=9&e=14&f=2016&g=d&ignore=.csv\"}";
+curl --header "$CONTENT_TYPE" -X POST --data "$DATA4" "$PYAF_URL"
+
+EXOG="https://raw.githubusercontent.com/antoinecarme/pyaf/master/data/ozone-la-exogenous-3.csv"
+DATA5="{\"Name\":\"model2_exog\",  \"Horizon\":\"21\", \"Present\":\"1969-01\", \"CSVFile\":\""$CSV"\", \"DateFormat\":\"%Y-%m\", \"ExogenousData\":\""$EXOG"\"}"
+echo $DATA5
+curl --header "$CONTENT_TYPE" -X POST --data "$DATA5" "$PYAF_URL"
+
+DATA6="{\"Name\":\"model2_no_exog\",  \"Horizon\":\"21\", \"Present\":\"1969-01\", \"CSVFile\":\""$CSV"\", \"DateFormat\":\"%Y-%m\"}"
+echo $DATA6
+curl --header "$CONTENT_TYPE" -X POST --data "$DATA6" "$PYAF_URL"
