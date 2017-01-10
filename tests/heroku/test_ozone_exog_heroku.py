7eb6760ace4a94cf79a25c5db800d23ab1120173
Antoine Carme <antoine.carme@laposte.net>
2017-01-10 14:08:32 +0100

    Allow Modeling with Exogenous Data through the REST API #28
    Analysis script

---
+++ tests/heroku/test_ozone_exog_heroku.py
@@ -0,0 +1,17 @@
+import pyaf.tests.heroku.build_generic_heroku_model as builder
+
+# this is a copy-paste from the API post data
+lDict = {
+    "CSVFile": "https://raw.githubusercontent.com/antoinecarme/TimeSeriesData/master/ozone-la.csv",
+    "DateFormat": "%Y-%m", 
+    "Horizon": 12, 
+    "Name": "model_ozone_exog", 
+    "Present": "", 
+    "SignalVar": "Ozone", 
+    "TimeVar": "Month",
+    "ExogenousData":"https://raw.githubusercontent.com/antoinecarme/pyaf/master/data/ozone-la-exogenous-3.csv",
+    # "ExogenousData":"data/ozone-la-exogenous-3.csv",
+    }
+
+print(sorted(lDict.items()))
+builder.build_model(lDict)
