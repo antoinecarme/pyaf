8c44200005c745a688354f5d320db80b00e3b192
Antoine Carme <antoine.carme@laposte.net>
2017-01-12 12:18:54 +0100

    Try to reduce memory usage #29
    Analysis scripts.

---
+++ tests/perf/test_perf2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+import TS.Perf as tsperf
+
+def test_perf(x , y):
+    lPerf = tsperf.cPerf();
+    lPerf.compute(x, y, "xxx");
+    print(x.shape , lPerf.mL2 , lPerf.mMAPE);
+
+
+for n in range(200, 1000000, 10000):
+    df = pd.DataFrame();
+    df['x'] = range(n);
+    df['y'] = np.sin(df['x']);
+    test_perf(df.x, df.y)
+    
