c2c3c5666340efea515f9a6879395dd59e9e5a80
Antoine Carme <antoine.carme@laposte.net>
2017-01-12 22:07:18 +0100

    Try to reduce memory usage #29
    Add a specific directory for this isssue tests.

---
+++ tests/bugs/issue_29/test_mem_1.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+
+import pandas as pd
+import numpy as np
+
+
+def display_used_mem():
+    print("DISPLAY_USED_MEM_START");
+    import gc
+    gc.collect()
+    import objgraph
+    objgraph.show_most_common_types(limit=20)
+    print("DISPLAY_USED_MEM_END");
+    
+
+# import '.' as pyaf_new_name
+# pyaf=pyaf_new_name
+# from pyaf
+
+display_used_mem();
+import Bench.TS_datasets as tsds
+
+#get_ipython().magic('matplotlib inline')
+
+display_used_mem();
+b1 = tsds.load_ozone()
+df = b1.mPastData
+
+display_used_mem();
+df.info();
+
+display_used_mem();
+
