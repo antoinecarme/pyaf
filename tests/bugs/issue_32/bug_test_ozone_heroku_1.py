2019a1ddcada3ab06141a12fabe042dafb291afb
Antoine Carme <antoine.carme@laposte.net>
2017-01-17 20:36:51 +0100

    Improve numerical stability #32
    Analysis scripts.

---
+++ tests/bugs/issue_32/bug_test_ozone_heroku_1.py
@@ -0,0 +1,14 @@
+import pyaf.tests.heroku.build_generic_heroku_model as builder
+
+# this is a copy-paste from the API post data
+lDict = {
+        "CSVFile": "https://raw.githubusercontent.com/antoinecarme/TimeSeriesData/master/ozone-la.csv",
+        "DateFormat": "%Y-%m", 
+        "Horizon": 12, 
+        "Name": "model1", 
+        "Present": "1968-08", 
+        "SignalVar": "Ozone", 
+        "TimeVar": "Month"
+      }
+
+builder.build_model(lDict)
---
+++ tests/bugs/issue_32/bug_test_ozone_heroku_1_ws.py
@@ -0,0 +1,33 @@
+# PYAF_URL="http://pyaf.herokuapp.com/model"
+PYAF_URL="http://0.0.0.0:8081/model"
+
+def test_heroku_pyaf(data):
+    url = PYAF_URL
+    header = {"Content-Type":"application/json"}
+
+    import httplib2 
+    http = httplib2.Http()
+    response, send = http.request(url, "POST", headers=header, body=data)
+    content = response.read()
+    return content;
+
+
+def test_heroku_pyaf_2(data):
+    import json, urllib3
+
+    http = urllib3.PoolManager()
+
+    r = http.request('POST', PYAF_URL,
+                     headers={'Content-Type': 'application/json'},
+                     body=json.dumps(data))
+
+    content = r.data
+    content = content.decode('utf-8').replace("\\n" , "\n");
+    return content;
+
+CSV="https://raw.githubusercontent.com/antoinecarme/TimeSeriesData/master/ozone-la.csv"
+data={"Name":"model1", "CSVFile":CSV, "DateFormat":"%Y-%m", "Present": "1968-08", "Horizon":21}
+cont = test_heroku_pyaf_2(data);
+print(cont);
+
+
