142228393a4408bb1fa45a60a08425d221782a5d
Antoine Carme <antoine.carme@laposte.net>
2017-02-20 13:21:10 +0100

    Store version information in Model metadata #36
    Added mTrainingVersionInfo in the best model.

--- TS/TimeSeriesModel.py
+++ TS/TimeSeriesModel.py
@@ -28,6 +28,7 @@ class cTimeSeriesModel:
         self.mTimeInfo = self.mTrend.mTimeInfo;
         self.mTime = self.mTimeInfo.mTime;
         self.mSignal = self.mTimeInfo.mSignal;
+        self.mTrainingVersionInfo = self.getVersions();
 
     def info(self):
         lSignal = self.mTrend.mSignalFrame[self.mSignal];
@@ -276,3 +277,39 @@ class cTimeSeriesModel:
                                                                                       lForecastColumn + '_Upper_Bound',
                                                                                       name = "prediction_intervals");
         return lDict;
+
+
+    def getVersions(self):
+        
+        import os, platform
+        lVersionDict = {};
+        lVersionDict["system_platform"] = platform.platform();
+        lVersionDict["system_uname"] = platform.uname();
+        lVersionDict["system_processor"] = platform.processor();
+        lVersionDict["python_implementation"] = platform.python_implementation();
+        lVersionDict["python_version"] = platform.python_version();
+
+        import sklearn
+        lVersionDict["sklearn_version"] = sklearn.__version__;
+
+        import pandas as pd
+        lVersionDict["pandas_version"] = pd.__version__;
+    
+        import numpy as np
+        lVersionDict["numpy_version"] = np.__version__;
+    
+        import scipy as sc
+        lVersionDict["scipy_version"] = sc.__version__;
+
+        import matplotlib
+        lVersionDict["matplotlib_version"] = matplotlib.__version__
+
+        import pydot
+        lVersionDict["pydot_version"] = pydot.__version__
+
+        import sqlalchemy
+        lVersionDict["sqlalchemy_version"] = sqlalchemy.__version__
+
+        # print([(k, lVersionDict[k]) for k in sorted(lVersionDict)]);
+        return lVersionDict;
+    
