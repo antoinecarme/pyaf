765148d7edb124a254400486889d804765f8a10e
Antoine Carme <antoine.carme@laposte.net>
2017-02-20 13:18:44 +0100

    Store version information in Model metadata #36
    Analysis script.

---
+++ tests/bugs/issue_36/issue_36_version_info.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+import pandas as pd
+import numpy as np
+
+
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
+
+
+b1 = tsds.load_ozone()
+df = b1.mPastData
+
+
+lEngine = autof.cForecastEngine()
+lEngine
+
+H = b1.mHorizon;
+lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
+lEngine.getModelInfo();
+
+print(lEngine.mSignalDecomposition.mBestModel.mTrainingVersionInfo);
