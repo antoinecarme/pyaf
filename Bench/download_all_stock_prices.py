f194b5571c92f8fcc7e4ece142c6b4bbfd0c367b
Antoine Carme <antoine.carme@laposte.net>
2017-03-09 18:48:46 +0100

    SignalDecomposition and TS_datasets missing? #37 Another copy of the same file ?

--- Bench/download_all_stock_prices.py
+++ Bench/download_all_stock_prices.py
@@ -6,7 +6,6 @@
 
 import pandas as pd
 import numpy as np
-import SignalDecomposition as SigDec
-import TS_datasets as tsds
+from Bench import TS_datasets as tsds
 
 tsds.download_yahoo_stock_prices();
