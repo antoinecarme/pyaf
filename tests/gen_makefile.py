9a2f62741aa5746af539903319e834ea1c4b7d30
Antoine Carme <antoine.carme@laposte.net>
2017-04-10 13:30:16 +0200

    Benchmark : expsmooth R package Datasets #40
    Updated test makefile

--- tests/gen_makefile.py
+++ tests/gen_makefile.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-str1 = "artificial bugs exog HeartRateTimeSeries heroku hierarchical model_control perf svr transformations bench func neuralnet real-life  time_res";
+str1 = "artificial bugs exog expsmooth HeartRateTimeSeries heroku hierarchical model_control perf svr transformations bench func neuralnet real-life  time_res";
 subdirs = str1.split();
 
 print("PYTHON=python3\n\n");
