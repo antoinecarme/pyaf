f2bd4e18571a1a01787923dea9617eb94caf4032
Antoine Carme <antoine.carme@laposte.net>
2017-04-23 00:12:30 +0200

    Added a missing import

--- tests/bench/test_yahoo.py
+++ tests/bench/test_yahoo.py
@@ -1,4 +1,5 @@
 import pyaf.Bench.YahooStocks as ys
+import pyaf.Bench.TS_datasets as tsds
 
 tester7 = ys.cYahoo_Tester(tsds.load_yahoo_stock_prices("my_test") , "YAHOO_my_test");
 tester7.testAllSignals(12);
