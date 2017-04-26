a485cc607540d4b87d1d85941d04e3b9fc8769e4
Antoine Carme <antoine.carme@laposte.net>
2017-04-26 11:03:49 +0200

    Benchmarking Process #45 split M4 competition by series type

--- benches/run_benchmark_M4Comp.py
+++ benches/run_benchmark_M4Comp.py
@@ -4,10 +4,14 @@ import Bench.MComp as mcomp
 import Bench.TS_datasets as tsds
 import warnings
 
-tester7 = mcomp.cMComp_Tester(tsds.load_M4_comp() , "M4_COMP");
-with warnings.catch_warnings():
-    warnings.simplefilter("error")
-    # tester7.testSignal('ECON0137')
-    # tester7.testSignal('ECON0299')
-    # tester7.testAllSignals()
-    tester7.run_multiprocessed(20);
+lTypes = ["BUSINESS-INDUSTRY", "DEMOGRAPHICS", "FINANCE", "INVENTORY", "CLIMATE",  "ECONOMICS", "INTERNET-TELECOM"]
+# lTypes = ["BUSINESS-INDUSTRY"];
+
+for ty in lTypes:
+    tester = mcomp.cMComp_Tester(tsds.load_M4_comp(ty) , "M4_COMP_" + ty);
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        # tester7.testSignal('ECON0137')
+        # tester7.testSignal('ECON0299')
+        # tester7.testAllSignals()
+        tester.run_multiprocessed();
