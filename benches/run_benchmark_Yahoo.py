ae139d2f0bc49b430880055f0ceed50a19de5217
Antoine Carme <antoine.carme@laposte.net>
2017-04-27 14:36:38 +0200

    Benchmarking Process #45 Updated this script

--- benches/run_benchmark_Yahoo.py
+++ benches/run_benchmark_Yahoo.py
@@ -4,6 +4,8 @@ import pyaf.Bench.TS_datasets as tsds
 import pyaf.Bench.YahooStocks as ys
 
 symbol_lists = tsds.get_yahoo_symbol_lists();
-for k in symbol_lists.keys():
+y_keys = sorted(symbol_lists.keys()) 
+print(y_keys)
+for k in y_keys:
     tester = ys.cYahoo_Tester(tsds.load_yahoo_stock_prices(k) , "YAHOO_STOCKS_" + k);
-    tester.run_multiprocessed(10);
+    tester.run_multiprocessed();
