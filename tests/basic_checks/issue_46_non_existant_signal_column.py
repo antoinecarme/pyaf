d97afaeb44116706febf12295e3bc7f9df658234
Antoine Carme <antoine.carme@laposte.net>
2017-04-28 17:06:53 +0200

    Enforce type checking for time and signal columns #47 Updated these tests

--- tests/basic_checks/issue_46_non_existant_signal_column.py
+++ tests/basic_checks/issue_46_non_existant_signal_column.py
@@ -3,7 +3,16 @@ import pandas as pd
 import pyaf.ForecastEngine as autof
 
 
-df = pd.DataFrame([[0 , 0.54543]], columns = ['date' , 'signal'])
-lEngine = autof.cForecastEngine()
-lEngine.train(df , 'date' , 'signal', 1);
+try:
+    df = pd.DataFrame([[0 , 0.54543]], columns = ['date' , 'signal'])
+    lEngine = autof.cForecastEngine()
+    lEngine.train(df , 'date' , 'signal_non_existant', 1);
+    raise Exception("NOT_OK")
+except Exception as e:
+    # should fail
+    print(str(e));
+    assert(str(e) == "SIGNAL_COLUMN_NOT_FOUND signal_non_existant")
+    if(str(e) == "NOT_OK"):
+        raise
+    pass
 
--- tests/basic_checks/issue_46_non_existant_time_column.py
+++ tests/basic_checks/issue_46_non_existant_time_column.py
@@ -2,8 +2,15 @@ import numpy as np
 import pandas as pd
 import pyaf.ForecastEngine as autof
 
-
-df = pd.DataFrame([[0 , 0.54543]], columns = ['date' , 'signal'])
-lEngine = autof.cForecastEngine()
-lEngine.train(df , 'date_other' , 'signal', 1);
-
+try:
+    df = pd.DataFrame([[0 , 0.54543]], columns = ['date' , 'signal'])
+    lEngine = autof.cForecastEngine()
+    lEngine.train(df , 'date_other' , 'signal', 1);
+    raise Exception("NOT_OK")
+except Exception as e:
+    # should fail
+    print(str(e));
+    assert(str(e) == "TIME_COLUMN_NOT_FOUND date_other")
+    if(str(e) == "NOT_OK"):
+        raise
+    pass
