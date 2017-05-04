77d8f11c94632c8266b509dcd37746380d0186b1
Antoine Carme <antoine.carme@laposte.net>
2017-05-04 21:41:32 +0200

    update hostname for travis-ci machines

--- scripts/upload_to_chunk.io.py
+++ scripts/upload_to_chunk.io.py
@@ -1,7 +1,7 @@
 import os
 myhost = os.uname()[1]
 print(myhost)
-if("travisci.net" in myhost):
+if("testing-docker" in myhost):
     print("on travis-ci")
     os.system("tar cvfz last_logs.tar.gz logs/")
     os.system("curl -s -T last_logs.tar.gz chunk.io")
