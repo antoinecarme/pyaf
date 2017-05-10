8e44bbd645995e0c5edfdd10c866876596c27b56
Antoine Carme <antoine.carme@laposte.net>
2017-05-10 12:13:26 +0200

    Add some tests for pyaf models serialization #51 Added a pickling step to this test collection

--- tests/model_control/test_ozone_custom_models_enabled.py
+++ tests/model_control/test_ozone_custom_models_enabled.py
@@ -6,6 +6,15 @@ import pyaf.Bench.TS_datasets as tsds
 #get_ipython().magic('matplotlib inline')
 
 
+def pickleModel(iModel):
+    import pickle
+    output = pickle.dumps(iModel)
+    lReloadedObject = pickle.loads(output)
+    output2 = pickle.dumps(lReloadedObject)    
+    assert(output2 == output2)
+    return lReloadedObject;
+
+
 def build_model(transformations, trends, periodics, autoregs):
     b1 = tsds.load_ozone_exogenous()
     df = b1.mPastData
@@ -24,16 +33,18 @@ def build_model(transformations, trends, periodics, autoregs):
     lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
     lEngine.getModelInfo();
     print(lEngine.mSignalDecomposition.mTrPerfDetails.head());
+
+    lEngine2 = pickleModel(lEngine)
     
-    lEngine.mSignalDecomposition.mBestModel.mTimeInfo.mResolution
+    lEngine2.mSignalDecomposition.mBestModel.mTimeInfo.mResolution
     
-    lEngine.standrdPlots("outputs/my_ozone");
+    lEngine2.standrdPlots("outputs/my_ozone");
     
     dfapp_in = df.copy();
     dfapp_in.tail()
 
     #H = 12
-    dfapp_out = lEngine.forecast(dfapp_in, H);
+    dfapp_out = lEngine2.forecast(dfapp_in, H);
     #dfapp_out.to_csv("outputs/ozone_apply_out.csv")
     dfapp_out.tail(2 * H)
     print("Forecast Columns " , dfapp_out.columns);
@@ -42,7 +53,7 @@ def build_model(transformations, trends, periodics, autoregs):
     print("Forecasts\n" , Forecast_DF.tail(H));
 
     print("\n\n<ModelInfo>")
-    print(lEngine.to_json());
+    print(lEngine2.to_json());
     print("</ModelInfo>\n\n")
     print("\n\n<Forecast>")
     print(Forecast_DF.tail(2*H).to_json(date_format='iso'))
