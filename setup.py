e19ad91d01b110dd18ec9208d1f053fd82f3c82f
Antoine Carme <antoine.carme@laposte.net>
2017-05-11 18:51:36 +0200

    Installation should be easier. #30 WIP

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
-#empty
+from setuptools import setup
+from setuptools import find_packages
 
+
+setup(name='pyaf',
+      version='1.0',
+      description='Python Automatic Forecasting',
+      author='Antoine CARME',
+      author_email='antoine.carme@laposte.net'
+      url='https://github.com/antoinecarme/pyaf'
+      license='BSD 3-clause',
+      packages=find_packages())
