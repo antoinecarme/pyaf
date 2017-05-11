8a8c7724b347a0e91c9f552fa36362452d013f1c
CARME Antoine <Antoine.Carme@Laposte.net>
2017-05-12 00:37:10 +0200

    Remvoed thsi file. No longer used.

--- setup_python_path.sh
+++
@@ -1,5 +0,0 @@
-# This script must be run from the pyaf directory to setup the path to the pyaf modules.
-# the goal is only to add the 'pyaf' parent directory in the PYTHONPATH envioronemnt variable.
-export PYTHONPATH=$PYTHONPATH:`pwd`/..
-
-
