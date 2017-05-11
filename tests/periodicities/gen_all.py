1d2a74e3792996fdc3a46f42dda80ad90bb84a2b
Antoine Carme <antoine.carme@laposte.net>
2017-05-11 17:53:27 +0200

    Add a periodicity test collection #52 Scripts used to generate asll posible periodic components and related datasets
    Business_Day, Business_Hour, Day, Hour, Minute, Month, Second, Semi_Month, Week

---
+++ tests/periodicities/gen_all.py
@@ -0,0 +1,53 @@
+import os
+
+# exit()
+
+def createDirIfNeeded(dirname):
+    try:
+        os.mkdir(dirname);
+    except:
+        pass
+
+createDirIfNeeded("tests/artificial");
+
+def gen_file(cyc , freq, nbrows):
+    lDir = "tests/periodicities/" + freq[1]; 
+    createDirIfNeeded(lDir)
+    filename = lDir + "/Cycle_" + str(freq[1]) + "_" +str(nbrows) + "_" + str(freq[0]) + "_" + str(cyc) + ".py";
+    file = open(filename, "w");
+    print("WRTITING_FILE" , filename);
+    file.write("import pyaf.tests.periodicities.period_test as per\n\n");
+    file.write("per.buildModel((" + str(cyc) + " , '" + str(freq[0]) + "' , " + str(nbrows) + "));\n\n");
+    file.close();
+    
+
+
+def generate_all():
+
+    lCycles = [ 5 , 7 , 12 , 15, 24, 30 , 60, 120, 360];
+    lFreqs = [('S' , 'Second'),
+              ('T', 'Minute'),
+              ('H' , 'Hour'),
+              ('BH', 'Business_Hour'),
+              ('D' , 'Day'),
+              ('B', 'Business_Day'),
+              ('W' , 'Week'),
+              ('SM', 'Semi_Month'),
+              ('M' , 'Month')]
+
+    for cyc in lCycles:
+        for freq in lFreqs:
+            lRows = [25, 50, 100, 200, 400, 1600, 3200];
+            if(freq[0] == 'W'):
+                lRows = [25, 50, 100, 200, 400, 1600];
+            if(freq[0] == 'SM'):
+                lRows = [25, 50, 100, 200, 400];
+            if(freq[0] == 'M'):
+                lRows = [25, 50, 100, 200, 400];
+            for nbrows in lRows:
+                gen_file(cyc , freq, nbrows);
+
+    
+
+
+generate_all();
