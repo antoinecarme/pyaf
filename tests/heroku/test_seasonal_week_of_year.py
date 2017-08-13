5ce46c93e66c9b38545418c2f3b79b8cea599118
Antoine Carme <antoine.carme@laposte.net>
2017-08-13 12:11:12 +0200

    Yahoo stopped its financial dat service. Updating this script

--- tests/heroku/test_seasonal_week_of_year.py
+++ tests/heroku/test_seasonal_week_of_year.py
@@ -1,11 +1,14 @@
 import pyaf.tests.heroku.build_generic_heroku_model as builder
 
 # this is a copy-paste from the API post data
+# swithchinfg to github data source .
+# Unfortunately, yahoo stopped its excellent financial data service ...
+
 lDict = {
-    "CSVFile": "http://chart.finance.yahoo.com/table.csv?s=GOOG&a=8&b=14&c=2015&d=9&e=14&f=2016&g=d&ignore=.csv", 
-    "DateFormat": "%Y-%m-%d", 
+    "CSVFile": "https://raw.githubusercontent.com/antoinecarme/TimeSeriesData/a5fa96af431caabc817180d52bc1d01b8c61da41/YahooFinance/nasdaq/yahoo_GOOG.csv",
+    "DateFormat": "%Y-%m-%d",
     "Horizon": 21, 
-    "Name": "PYAF_MODEL_DJOQFD", 
+    "Name": "PYAF_MODEL_DJOQFD_GOOG", 
     "Present": "2016-01-01", 
     "SignalVar": "Close", 
     "TimeVar": "Date"
