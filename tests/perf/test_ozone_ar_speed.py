b40cff24f034bb29c7800046f4a75a76834e1ff5
Antoine Carme <antoine.carme@laposte.net>
2017-09-18 17:32:25 +0200

    Optimize seasonal pattern extraction #70
    Added two scripts to benchamrk AR modeling.

---
+++ tests/perf/test_ozone_ar_speed.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
+
+#get_ipython().magic('matplotlib inline')
+
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+
+b1 = tsds.load_ozone()
+
+def addWeeks(iTime , iWeeks):
+    lTime = iTime
+    date_after_week = lTime + relativedelta(weeks=iWeeks)
+    return np.datetime64(date_after_week) 
+
+def replicate(df , K):
+    N = df.shape[0];
+    y = pd.Series(range(K*N));
+    tmin = df.Time.min()
+    Time1 = y.apply(lambda x : addWeeks(tmin , x))
+    # Time1.describe()
+    Ozone1 = pd.concat([df.Ozone] * K)
+    print(Time1.shape, Ozone1.shape, Time1.describe(), Ozone1.describe())
+    df1 = pd.DataFrame();
+    df1['Time'] = Time1.values;
+    print(Ozone1.describe());
+    df1['Ozone'] = Ozone1.values;
+    return df1;
+
+
+def buildModel(df, ar_order, H):
+
+    lEngine = autof.cForecastEngine()
+
+    lEngine.mOptions.mMaxAROrder = ar_order;
+    lEngine.mOptions.mDebug = True;
+    lEngine.mOptions.mDebugProfile = True;
+    lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
+    lEngine.getModelInfo();
+
+
+def run_test():
+    df = b1.mPastData    
+    df1 = replicate(df, 50);
+    df1.head()
+    H = b1.mHorizon;
+    buildModel(df1 , 1000 , H)
+
+
+run_test()
