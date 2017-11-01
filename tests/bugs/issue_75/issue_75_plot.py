a37d0122cc25013b103e4476c62fe552af1079e0
Antoine Carme <antoine.carme@laposte.net>
2017-11-01 18:50:56 +0100

    Wrong columns in standard plots #75 Analysis scripts

---
+++ tests/bugs/issue_75/issue_75_plot.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+
+import pandas as pd
+import numpy as np
+
+
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
+
+
+b1 = tsds.load_ozone()
+df = b1.mPastData
+
+#df.tail(10)
+#df[:-10].tail()
+#df[:-10:-1]
+#df.describe()
+
+
+lEngine = autof.cForecastEngine()
+lEngine
+
+H = b1.mHorizon;
+# lEngine.mOptions.enable_slow_mode();
+# lEngine.mOptions.mDebugPerformance = True;
+lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
+lEngine.getModelInfo();
+print(lEngine.mSignalDecomposition.mTrPerfDetails.head());
+
+lEngine.mSignalDecomposition.mBestModel.mTimeInfo.mResolution
+
+try:
+    lEngine.standrdPlots("outputs/my_ozone_issue_75");
+    assert(0)
+except:
+    print("OK")
+
+lEngine.standardPlots("outputs/my_ozone_issue_75");
