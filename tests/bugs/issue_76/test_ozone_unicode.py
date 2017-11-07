93efbf12c261a161ef43eb302dc24fdac60411b6
Antoine Carme <antoine.carme@laposte.net>
2017-11-07 10:13:51 +0100

    Double check unicode support #76 Analysis script

---
+++ tests/bugs/issue_76/test_ozone_unicode.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
+
+
+b1 = tsds.load_ozone()
+df = b1.mPastData
+
+lTimeVar = u"月"
+lSignalVar = u"臭氧"
+df[lSignalVar] = df[b1.mSignalVar]
+df[lTimeVar] = df[b1.mTimeVar]
+
+
+lEngine = autof.cForecastEngine()
+lEngine
+
+H = b1.mHorizon;
+lEngine.train(df , lTimeVar , lSignalVar, H);
+lEngine.getModelInfo();
+print(lEngine.mSignalDecomposition.mTrPerfDetails.head());
+
+
+lEngine.standardPlots("outputs/issue_76_unicode__ozone");
+
+dfapp_in = df.copy();
+dfapp_in.tail()
+
+dfapp_out = lEngine.forecast(dfapp_in, H);
+dfapp_out.to_csv("outputs/issue_76_unicode_ozone_apply_out.csv")
+print("Forecast Columns " , dfapp_out.columns);
+print(dfapp_out.tail(2 * H))
+Forecast_DF = dfapp_out[[lTimeVar , lSignalVar, lSignalVar + '_Forecast']]
+print(Forecast_DF.info())
+print("Forecasts\n" , Forecast_DF.tail(H));
+
+print("\n\n<ModelInfo>")
+print(lEngine.to_json());
+print("</ModelInfo>\n\n")
+print("\n\n<Forecast>")
+print(Forecast_DF.tail(2*H).to_json(date_format='iso'))
+print("</Forecast>\n\n")
+
