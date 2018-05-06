66e4b21b7a4b497e76a424211b11a60018ad5f6a
Antoine Carme <antoine.carme@laposte.net>
2018-05-06 18:17:36 +0200

    Add an explicit error message about python 2 support
    Raise an exception with python 2 whern pyaf in imported.
    Only Python 3.x is supported !!!

--- __init__.py
+++ __init__.py
@@ -4,8 +4,15 @@
 # This file is part of the Python Automatic Forecasting (PyAF) library and is made available under
 # the terms of the 3 Clause BSD license
 
+def check_python_version_for_pyaf():
+    import six
+    if six.PY2:
+        raise Exception("PYAF_ERROR_PYTHON_2_NOT_SUPPORTED")
+
+
+check_python_version_for_pyaf()
+
 from . import ForecastEngine, HierarchicalForecastEngine
 
 __version__ = '1.0.0'
-
 
