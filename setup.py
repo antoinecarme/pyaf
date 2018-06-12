8b4ba0fbde42cbc374130a935b2e48f1a585d847
43669 <joshua.mabry@bain.com>
2018-06-12 10:09:44 -0700

    make package directory and simplify package setup

--- setup.py
+++ setup.py
@@ -4,32 +4,6 @@ from setuptools import find_packages
 from shutil import copyfile, rmtree
 import os
 import glob
-
-def build_package():
-    try:
-        rmtree('pyaf')
-    except:
-        pass
-    os.mkdir('pyaf')
-    os.mkdir('pyaf/TS')
-    for file in glob.glob('TS/*.py'):        
-        copyfile(file, 'pyaf/' + file)
-    copyfile('ForecastEngine.py' ,
-             'pyaf/ForecastEngine.py')
-    copyfile('HierarchicalForecastEngine.py',
-             'pyaf/HierarchicalForecastEngine.py')
-    copyfile('__init__.py',
-             'pyaf/__init__.py')
-    os.mkdir('pyaf/CodeGen')
-    for file in glob.glob('CodeGen/*.py'):        
-        copyfile(file, 'pyaf/' + file)
-    os.mkdir('pyaf/Bench')
-    for file in glob.glob('Bench/*.py'):        
-        copyfile(file, 'pyaf/' + file)
-    
-
-build_package();
-
 setup(name='pyaf',
       version='1.0',
       description='Python Automatic Forecasting',
@@ -37,7 +11,7 @@ setup(name='pyaf',
       author_email='antoine.carme@laposte.net',
       url='https://github.com/antoinecarme/pyaf',
       license='BSD 3-clause',
-      packages=['pyaf' , 'pyaf.TS' ,  'pyaf.CodeGen' ,  'pyaf.Bench'],
+      packages=['pyaf'],
       install_requires=[
           'scipy',
           'pandas',
@@ -49,4 +23,3 @@ setup(name='pyaf',
           'sqlalchemy'
       ])
 
-rmtree('pyaf')
