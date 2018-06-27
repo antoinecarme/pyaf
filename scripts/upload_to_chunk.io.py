672f4f6e20d666c8654d969ac73b845f7ad8229b
Antoine Carme <antoine.carme@laposte.net>
2018-06-27 12:40:09 +0200

    Updated this script

--- scripts/upload_to_chunk.io.py
+++ scripts/upload_to_chunk.io.py
@@ -1,7 +1,7 @@
 import os
 myhost = os.uname()[1]
 print(myhost)
-if("testing-docker" in myhost):
+if("travis-job" in myhost):
     print("on travis-ci")
     os.system("tar cvfz last_logs.tar.gz logs/")
     os.system("curl -s -T last_logs.tar.gz curldu.mp")
