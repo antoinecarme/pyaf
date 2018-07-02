217f69b67b200b74945c4a0e570db5dbdaa95921
Antoine Carme <antoine.carme@laposte.net>
2018-07-02 22:20:37 +0200

    Warning Hunt : Matplotlib wanings in travis-ci reports #94 analysis script

---
+++ tests/bugs/issue_94/issue_94.py
@@ -0,0 +1,42 @@
+
+
+import numpy as np
+import pandas as pd
+
+N = 360
+df_train = pd.DataFrame({"Date" : pd.date_range(start="2016-01-25", periods=N, freq='D'),
+                         "Signal" : (np.arange(N)//40 + np.arange(N) % 21 + np.random.randn(N))})
+
+import pyaf.ForecastEngine as autof
+# create a forecast engine. This is the main object handling all the operations
+lEngine = autof.cForecastEngine()
+
+# get the best time series model for predicting one week
+lEngine.train(iInputDS = df_train, iTime = 'Date', iSignal = 'Signal', iHorizon = 7);
+lEngine.getModelInfo() # => relative error 7% (MAPE)
+
+
+# In[6]:
+
+
+import warnings
+
+with warnings.catch_warnings():
+    warnings.simplefilter("error")
+    lEngine.standardPlots()
+
+
+# In[7]:
+
+
+
+# predict one week
+df_forecast = lEngine.forecast(iInputDS = df_train, iHorizon = 7)
+# list the columns of the forecast dataset
+print(df_forecast.columns) #
+
+# print the real forecasts
+print(df_forecast['Date'].tail(7).values)
+
+# signal forecast : [ 9.74934646  10.04419761  12.15136455  12.20369717  14.09607727 15.68086323  16.22296559]
+print(df_forecast['Signal_Forecast'].tail(7).values)
