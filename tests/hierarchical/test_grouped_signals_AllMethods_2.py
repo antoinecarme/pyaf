fd34d8862e2e214b0e97c8487958e3910780e1a5
Antoine Carme <antoine.carme@laposte.net>
2020-04-24 19:04:00 +0200

    Imporved/Hardened a previous grouped signals test

---
+++ tests/hierarchical/test_grouped_signals_AllMethods_2.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import pyaf.HierarchicalForecastEngine as hautof
+import pyaf.Bench.TS_datasets as tsds
+
+import datetime
+
+#get_ipython().magic('matplotlib inline')
+
+b1 = tsds.load_AU_infant_grouped_dataset();
+
+
+df = b1.mPastData;
+
+lEngine = hautof.cHierarchicalForecastEngine()
+lEngine.mOptions.mHierarchicalCombinationMethod =  ["BU" , 'TD' , 'MO' , 'OC'];
+lEngine.mOptions.mNbCores = 16
+lEngine
+
+H = b1.mHorizon;
+
+# lEngine.mOptions.enable_slow_mode();
+# lEngine.mOptions.mDebugPerformance = True;
+lEngine.mOptions.set_active_autoregressions([]);
+lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H, b1.mHierarchy, None);
+
+lEngine.getModelInfo();
+#lEngine.standardPlots("outputs/AU_infant_");
+
+dfapp_in = df.copy();
+dfapp_in.tail()
+
+dfapp_out = lEngine.forecast(dfapp_in, H);
+#dfapp_out.to_csv("outputs/Grouped_AU_apply_out.csv")
