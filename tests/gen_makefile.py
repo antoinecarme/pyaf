16f907b0cea484c9df7f48bf788fb91d79907f6b
Antoine Carme <antoine.carme@laposte.net>
2020-06-02 01:25:03 +0200

    Updated Makefile

--- tests/gen_makefile.py
+++ tests/gen_makefile.py
@@ -27,7 +27,7 @@ def add_makefile_entry(subdir1):
     return test_target;
 
 
-str1 = "artificial basic_checks bugs cross_validation croston exog expsmooth func HeartRateTimeSeries heroku hierarchical model_control perf svr transformations  neuralnet real-life  time_res perfs demos xgb xeon-phi-parallel sampling temporal_hierarchy";
+str1 = "artificial basic_checks bugs cross_validation croston exog expsmooth func HeartRateTimeSeries heroku hierarchical  HourOfWeek model_control perf svr transformations  neuralnet real-life  time_res perfs demos xgb xeon-phi-parallel sampling temporal_hierarchy WeekOfMonth";
 subdirs = str1.split();
 
 print("PYTHON=python3\n\n");
@@ -45,6 +45,6 @@ print("\n# ********************************************** \n");
 
 print("all: " , str1 , "\n\t\n");
 
-str2 = "demos basic_checks cross_validation croston exog heroku hierarchical model_control perfs svr transformations func real-life  time_res xgb sampling";
+str2 = "demos basic_checks cross_validation croston exog heroku hierarchical model_control perfs svr transformations func real-life  time_res xgb sampling HourOfWeek WeekOfMonth";
 
 print("build-test : " , str2 , "\n\t\n");
