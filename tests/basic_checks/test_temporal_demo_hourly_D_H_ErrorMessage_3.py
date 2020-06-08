eb1bbc052acefed6edaea3ae593d45368fdab5de
Antoine Carme <antoine.carme@laposte.net>
2020-06-08 02:37:19 +0200

    Add temporal hierarchical forecasting #127
    Added some tests for three new error messages

---
+++ tests/basic_checks/test_temporal_demo_hourly_D_H_ErrorMessage_3.py
@@ -0,0 +1,48 @@
+# %matplotlib inline
+import pyaf
+
+import numpy as np
+import pandas as pd
+
+DATA_FREQ = 'H'
+PERIODS = ["T" , "H"]
+H = 365
+N = H * 10
+lDateColumn = "Date"
+lSignalVar = "Signal";
+START_TIME = "2001-01-25"
+
+# generate a daily signal covering one year 2016 in a pandas dataframe
+np.random.seed(seed=1960)
+df_train = pd.DataFrame({lDateColumn : pd.date_range(start=START_TIME, periods=N, freq=DATA_FREQ),
+                         lSignalVar : (np.arange(N)//40 + np.arange(N) % 21 + np.random.randn(N))})
+
+# print(df_train.head(N))
+
+lHierarchy = {};
+lHierarchy['Levels'] = None;
+lHierarchy['Data'] = None;
+lHierarchy['Groups']= {};
+
+lHierarchy['Periods']= PERIODS
+
+lHierarchy['Type'] = "Temporal";
+
+
+# create a hierarchical model and train it
+import pyaf.HierarchicalForecastEngine as hautof
+
+lEngine = hautof.cHierarchicalForecastEngine()
+# lEngine.mOptions.mNbCores = 1
+lEngine.mOptions.mHierarchicalCombinationMethod = ["BU" , 'TD' , 'MO' , 'OC'];
+
+lFailed = None
+try:
+    lSignalHierarchy = lEngine.train(df_train , lDateColumn, lSignalVar, H, lHierarchy, None);
+    lFailed = False
+except Exception as lEx:
+    print("ERROR" ,  lEx.__class__, lEx)
+    lFailed = True
+
+if(not lFailed):
+    raise Exception("NORMAL_BEHAVIOR_NOT_EXPECTED_SHOULD_HAVE_FAILED")
