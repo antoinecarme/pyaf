a14191d0752e46425d620cf37ae888407244875e
Antoine Carme <antoine.carme@laposte.net>
2021-06-17 20:59:03 +0200

    Add LnQ Performance measure #43
    Add LnQ values in the model logging

--- pyaf/TS/TimeSeriesModel.py
+++ pyaf/TS/TimeSeriesModel.py
@@ -137,6 +137,7 @@ class cTimeSeriesModel:
         logger.info("MODEL_CRPS CRPS_Fit=" + str(self.mFitPerf.mCRPS) + " CRPS_Forecast=" + str(self.mForecastPerf.mCRPS)  + " CRPS_Test=" + str(self.mTestPerf.mCRPS) );
         logger.info("MODEL_L1 L1_Fit=" + str(self.mFitPerf.mL1) + " L1_Forecast=" + str(self.mForecastPerf.mL1)  + " L1_Test=" + str(self.mTestPerf.mL1) );
         logger.info("MODEL_L2 L2_Fit=" + str(self.mFitPerf.mL2) + " L2_Forecast=" + str(self.mForecastPerf.mL2)  + " L2_Test=" + str(self.mTestPerf.mL2) );
+        logger.info("MODEL_LnQ LnQ_Fit=" + str(self.mFitPerf.mLnQ) + " LnQ_Forecast=" + str(self.mForecastPerf.mLnQ)  + " LnQ_Test=" + str(self.mTestPerf.mLnQ) );
         logger.info("MODEL_MEDIAN_AE MedAE_Fit=" + str(self.mFitPerf.mMedAE) + " MedAE_Forecast=" + str(self.mForecastPerf.mMedAE)  + " MedAE_Test=" + str(self.mTestPerf.mMedAE) );
         logger.info("MODEL_COMPLEXITY " + str(self.getComplexity()) );
         logger.info("SIGNAL_TRANSFORMATION_DETAIL_START");
@@ -321,6 +322,7 @@ class cTimeSeriesModel:
               "MAE" : str(self.mForecastPerf.mL1),
               "RMSE" : str(self.mForecastPerf.mL2),
               "MedAE" : str(self.mForecastPerf.mMedAE),
+              "LnQ" : str(self.mForecastPerf.mLnQ),
               "COMPLEXITY" : str(self.getComplexity())};
         dict1["Model_Performance"] = d3;
         if(iWithOptions):
