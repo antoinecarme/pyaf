4d2368653e4be88fb92edb30300c771cd8f31a7a
Antoine Carme <antoine.carme@laposte.net>
2022-04-07 04:18:42 +0200

    Corrected this benchmark launching script.

--- scripts/run_all_benches.sh
+++ scripts/run_all_benches.sh
@@ -3,13 +3,13 @@ mkdir logs
 # The output of the benchmarks will be in logs directory.
 # This script is used for bernchmarks , not for tests.
 # tests are located in the 'tests' directory. There is a tests/Makefile.
-ipython3 benches/run_benchmark_artificial.py
-ipython3 benches/run_benchmark_M1Comp.py
-ipython3 benches/run_benchmark_M2Comp.py
-ipython3 benches/run_benchmark_M3Comp.py
-ipython3 benches/run_benchmark_M4Comp.py
-ipython3 benches/run_benchmark_MComp.py
-ipython3 benches/run_benchmark_MWH.py
-ipython3 benches/run_benchmark_NN3.py
-ipython3 benches/run_benchmark_NN5.py
-ipython3 benches/run_benchmark_Yahoo.py
+python3 benches/run_benchmark_artificial.py
+python3 benches/run_benchmark_M1Comp.py
+python3 benches/run_benchmark_M2Comp.py
+python3 benches/run_benchmark_M3Comp.py
+python3 benches/run_benchmark_M4Comp.py
+python3 benches/run_benchmark_MComp.py
+python3 benches/run_benchmark_MWH.py
+python3 benches/run_benchmark_NN3.py
+python3 benches/run_benchmark_NN5.py
+python3 benches/run_benchmark_Yahoo.py
