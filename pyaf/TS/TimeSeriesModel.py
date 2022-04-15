e209e5613e7b0732fbfaf0e08b58a75b9a977d2f
Antoine Carme <antoine.carme@laposte.net>
2022-04-15 20:47:38 +0200

    Refactored used module version methods. For debugging purposes.

--- pyaf/TS/TimeSeriesModel.py
+++ pyaf/TS/TimeSeriesModel.py
@@ -436,36 +436,6 @@ class cTimeSeriesModel:
             
         return lDict;
 
-
     def getVersions(self):
-        
-        import os, platform, pyaf
-        lVersionDict = {};
-        lVersionDict["PyAF_version"] = pyaf.__version__;
-        lVersionDict["system_platform"] = platform.platform();
-        lVersionDict["system_uname"] = platform.uname();
-        lVersionDict["system_processor"] = platform.processor();
-        lVersionDict["python_implementation"] = platform.python_implementation();
-        lVersionDict["python_version"] = platform.python_version();
-
-        import sklearn
-        lVersionDict["sklearn_version"] = sklearn.__version__;
-
-        import pandas as pd
-        lVersionDict["pandas_version"] = pd.__version__;
-    
-        import numpy as np
-        lVersionDict["numpy_version"] = np.__version__;
-    
-        import scipy as sc
-        lVersionDict["scipy_version"] = sc.__version__;
-
-        import matplotlib
-        lVersionDict["matplotlib_version"] = matplotlib.__version__
-
-        import pydot
-        lVersionDict["pydot_version"] = pydot.__version__
-
-        # print([(k, lVersionDict[k]) for k in sorted(lVersionDict)]);
+        lVersionDict = tsutil.getVersions();
         return lVersionDict;
-    
