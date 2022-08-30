17cc126948c475bbb8e318b6d1b71f5fb3b51cc7
Antoine CARME <Antoine.Carme@outlook.com>
2022-08-30 18:41:16 +0200

    Bad plot for shaded area around prediction intervals in hourly data #216. Fixed

--- pyaf/TS/Plots.py
+++ pyaf/TS/Plots.py
@@ -124,11 +124,7 @@ def prediction_interval_plot_internal(df, time, signal, estimator, lower, upper,
                   ax=axs, grid = True, legend=False)
     add_patched_legend(axs , [signal, estimator, lower, upper])
 
-    x = df1[time];
-    type1 = x.dtype
-    if(type1.kind == 'M'):
-        x = x.apply(lambda t : t.date());
-    axs.fill_between(x.values, df1[lower], df1[upper], color=SHADED_COLOR, alpha=.2)
+    axs.fill_between(df1[time].values, df1[lower], df1[upper], color=SHADED_COLOR, alpha=.2)
 
     return fig
 
