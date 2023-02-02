2e373151550c5107f3df3083754784362f56c411
Antoine CARME <antoine.carme@outlook.com>
2023-02-02 18:19:59 +0100

    Double check unicode support #76. Updated this script. Define default fonts before building the model.

--- tests/bugs/issue_76/test_ozone_unicode.py
+++ tests/bugs/issue_76/test_ozone_unicode.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = ['Segoe UI Emoji', 'SimHei']
 
 import pyaf.ForecastEngine as autof
 import pyaf.Bench.TS_datasets as tsds
