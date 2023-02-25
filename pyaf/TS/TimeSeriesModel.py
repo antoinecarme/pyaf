d1f245047ef86e5894ce8dabf5daff2d74136d60
Antoine CARME <antoine.carme@outlook.com>
2023-02-26 00:46:18 +0100

    Add Differentiable Variant of SMAPE Performance Measure #221. Added new perf measure values in the logs.

--- pyaf/TS/TimeSeriesModel.py
+++ pyaf/TS/TimeSeriesModel.py
@@ -145,6 +145,7 @@ class cTimeSeriesModel:
         logger.info("AUTOREG_DETAIL '" + self.mAR.mOutName + "' [" + self.mAR.mFormula + "]");
         logger.info("MODEL_MAPE MAPE_Fit=" + str(self.mFitPerf.mMAPE) + " MAPE_Forecast=" + str(self.mForecastPerf.mMAPE)  + " MAPE_Test=" + str(self.mTestPerf.mMAPE) );
         logger.info("MODEL_SMAPE SMAPE_Fit=" + str(self.mFitPerf.mSMAPE) + " SMAPE_Forecast=" + str(self.mForecastPerf.mSMAPE)  + " SMAPE_Test=" + str(self.mTestPerf.mSMAPE) );
+        logger.info("MODEL_DiffSMAPE DiffSMAPE_Fit=" + str(self.mFitPerf.mDiffSMAPE) + " DiffSMAPE_Forecast=" + str(self.mForecastPerf.mDiffSMAPE)  + " DiffSMAPE_Test=" + str(self.mTestPerf.mDiffSMAPE) );
         logger.info("MODEL_MASE MASE_Fit=" + str(self.mFitPerf.mMASE) + " MASE_Forecast=" + str(self.mForecastPerf.mMASE)  + " MASE_Test=" + str(self.mTestPerf.mMASE) );
         logger.info("MODEL_CRPS CRPS_Fit=" + str(self.mFitPerf.mCRPS) + " CRPS_Forecast=" + str(self.mForecastPerf.mCRPS)  + " CRPS_Test=" + str(self.mTestPerf.mCRPS) );
         logger.info("MODEL_L1 L1_Fit=" + str(self.mFitPerf.mL1) + " L1_Forecast=" + str(self.mForecastPerf.mL1)  + " L1_Test=" + str(self.mTestPerf.mL1) );
