aebc07c6fa7a04a412b8a501582a517f175b977f
Antoine CARME <antoine.carme@outlook.com>
2023-03-11 21:08:06 +0100

    Added artificially generated data (hourly and daily, by dataset size, just a backup, can be re-generated)

---
+++ tests/bench/test_artificial_bench_Signal_1000_H_0_linear_0_exp_1.0_10_12.py
@@ -0,0 +1,13 @@
+import pyaf.Bench.TS_datasets as tsds
+
+import pyaf.Bench.Artificial as art
+import warnings
+
+
+lSignal = "Signal_1000_H_0_linear_0_exp_1.0_10"
+
+
+with warnings.catch_warnings():
+    warnings.simplefilter("error")
+    tester1 = art.cArtificial_Tester(tsds.load_artificial_datsets("L", iName = lSignal) , "ARTIFICIAL_L");
+    tester1.testSignals(lSignal);
