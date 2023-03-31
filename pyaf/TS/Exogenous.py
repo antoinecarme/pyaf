2132f93b27ba1378d7fe5112134cbb1a56d57514
Antoine CARME <antoine.carme@outlook.com>
2023-03-31 18:53:48 +0200

    Add more info about Exogenous Data Used in ARX Models #236. to_dict() and info() Implementation. Some performance improvements (use numpy instead of pandas for detecting categories).

--- pyaf/TS/Exogenous.py
+++ pyaf/TS/Exogenous.py
@@ -23,15 +23,72 @@ class cExogenousInfo:
         self.mDateVariable = None;
         self.mContExogenousStats = None;
         self.mExcluded = [];
+
+    def long_list_to_str(self, iList):
+        lList_str = ""
+        if(len(iList) > 10):
+            lList_str = str(iList[:5]) + " ... " + str(iList[-5:])
+        else:
+            lList_str = str(iList)
+        return lList_str
+
+    def decode_used_vars(self, used):
+        used_vars = used
+        if(used is not None):
+            used_vars = [x.split("=")[0] for x in used]
+            used_vars = [x for x in used_vars if x in self.mExogenousVariables]
+            used_vars = list(set(used_vars))
+        return used_vars
+
+
+    def info(self, used = None):
+        logger = tsutil.get_pyaf_logger();
+        lDict = self.to_dict(used)
+        logger.info("EXOGENOUS_VARIABLE_DETAIL_START")
+        for exog in lDict["Categorical_Variables"].keys():
+            lList = lDict["Categorical_Variables"][exog]
+            lUsed_cat = lDict["Categorical_Variables_Usage"][exog]
+            logger.info("EXOGENOUS_VARIABLE_DETAIL_CATEGORICAL_FREQUENCIES '" + exog + "' " + str(lList))
+            logger.info("EXOGENOUS_VARIABLE_DETAIL_CATEGORICAL_USED '" + exog + "' " + str(lUsed_cat))
+        lExcluded_cat = lDict["Categorical_Variables_Excluded"]
+        logger.info("EXOGENOUS_VARIABLE_DETAIL_CATEGORICAL_EXCLUDED " + str(len(lExcluded_cat)) + " " + self.long_list_to_str(lExcluded_cat))
+        for (exog, lStats) in lDict["Continuous_Variables"].items():
+            logger.info("EXOGENOUS_VARIABLE_DETAIL_CONTINUOUS '" + exog + "' " + str(lStats))
+        lExcluded_cont = lDict["Continuous_Variables_Excluded"]
+        logger.info("EXOGENOUS_VARIABLE_DETAIL_CONTINUOUS_EXCLUDED " + str(len(lExcluded_cont)) + " " + self.long_list_to_str(lExcluded_cont))
         
-    def info(self):
-        lStr2 = "ExogenousVariables = '" + self.mExogD +"'";
-        return lStr2;
+        logger.info("EXOGENOUS_VARIABLE_DETAIL_END")
 
 
-    def to_dict(self):
+    def to_dict(self, used = None):
+        used_vars = self.decode_used_vars(used)
         dict1 = {};
+        lExcluded_cat = []
+        dict1 ["Categorical_Variables"] = {}
+        dict1 ["Categorical_Variables_Usage"] = {}
+        for (exog, lList) in self.mExogenousVariableCategories.items():
+            if(used_vars is not None and (exog in used_vars)):
+                lUsed_cat = []
+                for (cat, freq) in lList:
+                    lUsed_cat_i = [x for x in used if x.startswith(exog + "=" + str(cat))]
+                    if(len(lUsed_cat_i) > 0):
+                        lUsed_cat = lUsed_cat + [exog + "=" + str(cat)]
+                dict1 ["Categorical_Variables"][exog] = dict(lList)
+                dict1 ["Categorical_Variables_Usage"][exog] = lUsed_cat
+            else:
+                lExcluded_cat = lExcluded_cat + [exog]
+        dict1 ["Categorical_Variables_Excluded"] = lExcluded_cat
+        dict1 ["Continuous_Variables"] = {}
+        lExcluded_cont = []
+        for exog in self.mContExogenousStats.keys():
+            if(used_vars is not None and (exog in used_vars)):
+                lExogStats = self.mContExogenousStats[exog];
+                dict1 ["Continuous_Variables"][exog] = {"Mean" : lExogStats[0], "StdDev" : lExogStats[1]}
+            else:
+                lExcluded_cont = lExcluded_cont + [exog]
+        dict1 ["Continuous_Variables_Excluded"] = lExcluded_cont
         return dict1;
+
 
     def check(self):
         if(self.mExogenousData is not None):
@@ -73,34 +130,68 @@ class cExogenousInfo:
     def transformDataset(self, df):
         df1 = self.addVars(df);
         return df1;
+
+    def encode_exogenous_categorical(self, exog):
+        lSeries = self.mExogenousDataFrame[exog]
+        lList = self.mExogenousVariableCategories[exog];
+        lEncodedVars = {}
+        assert(lList is not None)
+        for lCat in lList:
+            lDummyName = exog + "=" + str(lCat[0]);
+            lEncodedVars[lDummyName] = np.where(lSeries == lCat[0] , np.int8(1), np.int8(0));
+        return lEncodedVars
         
+    def encode_exogenous_continuous(self, exog):
+        lSeries = self.mExogenousDataFrame[exog]
+        lEncodedVars = {}
+        lExogStats = self.mContExogenousStats[exog];
+        # single precision here ...
+        lStandardized = (lSeries - lExogStats[0])/ lExogStats[1];
+        lStandardized = lStandardized.astype(np.float32);
+        lStandardized.fillna(np.float32(0.0), inplace=True);
+        lEncodedVars[exog] = lStandardized
+        return lEncodedVars
+
+    
     def createEncodedExogenous(self):
         self.mEncodedExogenous = [];
         lEncodedVars = {self.mDateVariable : self.mExogenousDataFrame[self.mDateVariable]}
-        for exog in self.mExogenousVariables:
-            if(exog not in self.mExcluded):
-                lSeries = self.mExogenousDataFrame[exog]
-                lList = self.mExogenousVariableCategories[exog];
-                if(lList is not None):
-                    for lCat in lList:
-                        lDummyName = exog + "=" + str(lCat);
-                        lEncodedVars[lDummyName] = np.where(lSeries == lCat , np.int8(1), np.int8(0));
-                        self.mEncodedExogenous = self.mEncodedExogenous + [lDummyName];
-                else:
-                    lExogStats = self.mContExogenousStats[exog];
-                    # single precision here ...
-                    lStandardized = (lSeries - lExogStats[0])/ lExogStats[1];
-                    lStandardized = lStandardized.astype(np.float32);
-                    lStandardized.fillna(np.float32(0.0), inplace=True);
-                    lEncodedVars[exog] = lStandardized
-                    self.mEncodedExogenous = self.mEncodedExogenous + [exog];
-            else:
-                # tsutil.print_pyaf_detailed_info("EXCLUDED" , exog);
-                pass
+        lEncodedVars_exog = {}
+        for exog in self.mExogenousVariableCategories.keys():
+            lEncodedVars_exog_cat = self.encode_exogenous_categorical(exog)
+            lEncodedVars_exog.update(lEncodedVars_exog_cat)
+        for exog in self.mContExogenousStats.keys():
+            lEncodedVars_exog_cont = self.encode_exogenous_continuous(exog)
+            lEncodedVars_exog.update(lEncodedVars_exog_cont)
+        for(lName, lEncodedSeries) in lEncodedVars_exog.items():
+            lEncodedVars[lName] = lEncodedSeries
+            self.mEncodedExogenous = self.mEncodedExogenous + [lName];
         self.mEncodedExogenousDataFrame = pd.DataFrame(lEncodedVars,
                                                        index = self.mExogenousDataFrame.index);
         # self.mEncodedExogenousDataFrame.info()
 
+    def updateExogenousVariableInfo_Categorical(self, exog, series):
+        NCat = self.mOptions.mMaxExogenousCategories;
+        # use nan as a category
+        lValues, lCounts = np.unique(series, return_counts=True)
+        lValueCounts = zip([x for x in lValues], [x for x in lCounts])
+        lValueCounts_Significant = [x for x in lValueCounts if(x[1] > 5)]
+        lValueCounts_ordered = sorted(lValueCounts_Significant, key = lambda x : (-x[1], x[0]))
+        lList = lValueCounts_ordered[:NCat]
+        lList = [(x[0] , int(x[1])) for x in lList]
+        if(len(lList) <= 1):
+            self.mExcluded.append(exog);
+        else:
+            self.mExogenousVariableCategories[exog] = lList;
+        
+    def updateExogenousVariableInfo_Continuous(self, exog, series):
+        stat_exog = (series.mean(), series.std());
+        if(stat_exog[1] > 1e-5):
+            self.mContExogenousStats[exog] = stat_exog;
+        else:
+            self.mExcluded.append(exog);
+        
+        
     def updateExogenousVariableInfo(self):
         # self.mExogenousDataFrame.info()
         # tsutil.print_pyaf_detailed_info(self.mExogenousDataFrame.describe());
@@ -117,24 +208,6 @@ class cExogenousInfo:
             lType = self.mExogenousDataFrame[exog].dtype
             # tsutil.print_pyaf_detailed_info("EXOG_DTYPE" , exog, lType)
             if((lType == object) or (lType.name == 'category')):
-                # use nan as a category
-                lVC = lEstimFrame[exog].value_counts(dropna = False, sort=False);
-                lVC = lVC[lVC > 5]
-                lVC = lVC.reset_index().sort_values(by=[exog, 'index'], ascending=[False, True]);
-                NCat = self.mOptions.mMaxExogenousCategories;
-                lVC = lVC.head(NCat)
-                # tsutil.print_pyaf_detailed_info("EXOGENOUS_DATA", exog, lVC.columns, lVC.head(NCat));
-                lList = lVC['index'].values
-                lList = sorted(lList.tolist());
-                # tsutil.print_pyaf_detailed_info("most_frequent_categories_for" , exog, lList);
-                if(len(lList) <= 1):
-                    self.mExcluded.append(exog);
-                else:
-                    self.mExogenousVariableCategories[exog] = lList;
+                self.updateExogenousVariableInfo_Categorical(exog, lEstimFrame[exog])
             else:
-                self.mExogenousVariableCategories[exog] = None;
-                stat_exog = (lEstimFrame[exog].mean(), lEstimFrame[exog].std());
-                if(stat_exog[1] > 1e-5):
-                    self.mContExogenousStats[exog] = stat_exog;
-                else:
-                    self.mExcluded.append(exog);
+                self.updateExogenousVariableInfo_Continuous(exog, lEstimFrame[exog])
