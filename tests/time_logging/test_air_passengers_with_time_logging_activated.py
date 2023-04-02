aedc8180a164d7c984807d8de56ff04a8cd73fac
Antoine CARME <antoine.carme@outlook.com>
2023-04-02 15:17:38 +0200

    Disable Timing Loggers by default #237. Added tests with time logger activated.

---
+++ tests/time_logging/test_air_passengers_with_time_logging_activated.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+import pyaf
+pyaf.activate_timer_logging()
+
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
+
+
+pyaf.activate_timer_logging()
+
+b1 = tsds.load_airline_passengers()
+df = b1.mPastData
+
+df.head()
+
+
+lEngine = autof.cForecastEngine()
+lEngine
+
+H = b1.mHorizon;
+# lEngine.mOptions.enable_slow_mode();
+# lEngine.mOptions.mDebugPerformance = True;
+lEngine.mOptions.mParallelMode = True;
+lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
+lEngine.getModelInfo();
+
+
+lEngine.mSignalDecomposition.mBestModel.mTimeInfo.mResolution
+
+lEngine.standardPlots(name = "outputs/my_airline_passengers")
+
+dfapp_in = df.copy();
+dfapp_in.tail()
+
+#H = 12
+dfapp_out = lEngine.forecast(dfapp_in, H);
+dfapp_out.tail(2 * H)
+print("Forecast Columns " , dfapp_out.columns);
+lForecastColumnName = b1.mSignalVar + '_Forecast'
+Forecast_DF = dfapp_out[[b1.mTimeVar , b1.mSignalVar, lForecastColumnName , lForecastColumnName + '_Lower_Bound',  lForecastColumnName + '_Upper_Bound' ]]
+print(Forecast_DF.info())
+print("Forecasts\n" , Forecast_DF.tail(2*H));
+
+print("\n\n<ModelInfo>")
+print(lEngine.to_json());
+print("</ModelInfo>\n\n")
+print("\n\n<Forecast>")
+print(Forecast_DF.tail(2*H).to_json(date_format='iso'))
+print("</Forecast>\n\n")
+
+# lEngine.standardPlots(name = "outputs/airline_passengers")
---
+++ tests/time_logging/test_ozone_with_time_logging_activated.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+
+import pandas as pd
+import numpy as np
+
+import pyaf
+pyaf.activate_timer_logging()
+
+import pyaf.ForecastEngine as autof
+import pyaf.Bench.TS_datasets as tsds
+
+
+
+b1 = tsds.load_ozone()
+df = b1.mPastData
+
+#df.tail(10)
+#df[:-10].tail()
+#df[:-10:-1]
+#df.describe()
+
+
+lEngine = autof.cForecastEngine()
+lEngine
+
+H = b1.mHorizon;
+# lEngine.mOptions.enable_slow_mode();
+# lEngine.mOptions.mDebugPerformance = True;
+lEngine.train(df , b1.mTimeVar , b1.mSignalVar, H);
+lEngine.getModelInfo();
+
+
+lEngine.mSignalDecomposition.mBestModel.mTimeInfo.mResolution
+
+lEngine.standardPlots("outputs/my_ozone");
+
+dfapp_in = df.copy();
+dfapp_in.tail()
+
+#H = 12
+dfapp_out = lEngine.forecast(dfapp_in, H);
+#dfapp_out.to_csv("outputs/ozone_apply_out.csv")
+dfapp_out.tail(2 * H)
+print("Forecast Columns " , dfapp_out.columns);
+Forecast_DF = dfapp_out[[b1.mTimeVar , b1.mSignalVar, b1.mSignalVar + '_Forecast']]
+print(Forecast_DF.info())
+print("Forecasts\n" , Forecast_DF.tail(H));
+
+print("\n\n<ModelInfo>")
+print(lEngine.to_json());
+print("</ModelInfo>\n\n")
+print("\n\n<Forecast>")
+print(Forecast_DF.tail(2*H).to_json(date_format='iso'))
+print("</Forecast>\n\n")
+
