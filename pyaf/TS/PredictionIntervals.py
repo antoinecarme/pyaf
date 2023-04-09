d1677009df76f9d6e23558a44ed74e46ca99de54
Antoine CARME <antoine.carme@outlook.com>
2023-04-09 18:38:44 +0200

    Perf computations refactoring. Added detrended and deseasonalized signal columns in the forecast dataframe.

--- pyaf/TS/PredictionIntervals.py
+++ pyaf/TS/PredictionIntervals.py
@@ -11,6 +11,8 @@ from . import SignalDecomposition as sigdec
 
 from . import Perf as tsperf
 from . import Utils as tsutil
+from . import TimeSeries_Cutting as tscut
+
 
 class cPredictionIntervalsEstimator:
     
@@ -18,9 +20,9 @@ class cPredictionIntervalsEstimator:
         self.mModel = None;
         self.mSignalFrame = None
         self.mHorizon = -1;
-        self.mFitPerformances = {}
-        self.mForecastPerformances = {}
-        self.mTestPerformances = {}
+        self.mPerformances = {"whole" : {}, "detrended" : {}, "deseasonalized" : {}, "transformed" : {}}
+        for lType in self.mPerformances.keys():
+            self.mPerformances[lType] = {tscut.eDatasetType.Fit : {}, tscut.eDatasetType.Forecast : {},  tscut.eDatasetType.Test : {}}
         self.mComputeAllPerfs = True
 
     def compute_one_perf(self, signal, estimator, iHorizonName):
@@ -35,6 +37,73 @@ class cPredictionIntervalsEstimator:
             lDict = lPerf.computeCriterionValues(signal, estimator, lCriterions, iHorizonName);
             return lDict
         return lPerf
+
+    def compute_detrended_perfs(self, original_cut_dfs, forecast_cut_dfs, h):
+        lTransformedSignalColumn = self.mModel.mSignal;
+        lTrendColumn = self.mModel.mTrend.mOutName;
+        lSeasonalColumn = self.mModel.mCycle.mOutName;
+        lARColumn = self.mModel.mAR.mOutName;
+        lForecastColumn = str(self.mOriginalSignal) + "_Forecast";
+        lHorizonName = lForecastColumn + "_detrended_" + str(h + 1);
+        for (lDataset, lDF) in forecast_cut_dfs.items():
+            lOriginalDF = original_cut_dfs[lDataset]
+            if(lDF.shape[0] > 0):
+                lDetrendedForecast = lDF[lSeasonalColumn] + lDF[lARColumn]
+                lTarget = lOriginalDF[lTransformedSignalColumn] - lDF[lTrendColumn]
+                if(self.mModel.mDecompositionType == 'TSR'):
+                    lDetrendedForecast = lDF[lSeasonalColumn] * lDF[lARColumn]
+                lTarget = lOriginalDF[lTransformedSignalColumn] / lDF[lTrendColumn]
+                if(self.mModel.mDecompositionType == 'TS+R'):
+                    lDetrendedForecast = lDF[lARColumn]
+                    lTarget = lOriginalDF[lTransformedSignalColumn] / lDF[lTrendColumn]
+                self.mPerformances["detrended"][lDataset][lHorizonName] = self.compute_one_perf(lTarget, lDetrendedForecast, lHorizonName)
+
+
+    def compute_transformed_perfs(self, original_cut_dfs, forecast_cut_dfs, h):
+        lTransformedSignalColumn = self.mModel.mSignal;
+        lTrendColumn = self.mModel.mTrend.mOutName;
+        lSeasonalColumn = self.mModel.mCycle.mOutName;
+        lARColumn = self.mModel.mAR.mOutName;
+        lForecastColumn = str(self.mOriginalSignal) + "_Forecast";
+        lHorizonName = lForecastColumn + "_transformed_" + str(h + 1);
+
+        for (lDataset, lDF) in forecast_cut_dfs.items():
+            lOriginalDF = original_cut_dfs[lDataset]
+            if(lDF.shape[0] > 0):
+                self.mPerformances["transformed"][lDataset][lHorizonName] = self.compute_one_perf(lOriginalDF[lTransformedSignalColumn] , lDF[lTrendColumn] + lDF[lSeasonalColumn] + lDF[lARColumn], lHorizonName);
+
+
+    def compute_deseasonalized_perfs(self, original_cut_dfs, forecast_cut_dfs, h):
+        lTransformedSignalColumn = self.mModel.mSignal;
+        lTrendColumn = self.mModel.mTrend.mOutName;
+        lSeasonalColumn = self.mModel.mCycle.mOutName;
+        lARColumn = self.mModel.mAR.mOutName;
+        lForecastColumn = str(self.mOriginalSignal) + "_Forecast";
+        lHorizonName = lForecastColumn + "_deseasonalized_" + str(h + 1);
+        
+        for (lDataset, lDF) in forecast_cut_dfs.items():
+            lOriginalDF = original_cut_dfs[lDataset]
+            if(lDF.shape[0] > 0):
+                lDeseasonalizedForecast = lDF[lTrendColumn] + lDF[lARColumn]
+                lTarget = lOriginalDF[lTransformedSignalColumn] - lDF[lSeasonalColumn]
+                if(self.mModel.mDecompositionType == 'TSR'):
+                    lDeseasonalizedForecast = lDF[lTrendColumn] * lDF[lARColumn]
+                    lTarget = lOriginalDF[lTransformedSignalColumn] / lDF[lSeasonalColumn]
+                if(self.mModel.mDecompositionType == 'TS+R'):
+                    lDeseasonalizedForecast = lDF[lARColumn]
+                    lTarget = lOriginalDF[lTransformedSignalColumn] / lDF[lSeasonalColumn]
+                self.mPerformances["deseasonalized"][lDataset][lHorizonName] = self.compute_one_perf(lTarget, lDeseasonalizedForecast, lHorizonName);
+
+    def compute_whole_perfs(self, original_cut_dfs, forecast_cut_dfs, h):
+        lOriginalSignalColumn = self.mOriginalSignal;
+        lForecastColumn = str(self.mOriginalSignal) + "_Forecast";
+        lHorizonName = lForecastColumn + "_" + str(h + 1);
+        
+        for (lDataset, lDF) in forecast_cut_dfs.items():
+            lOriginalDF = original_cut_dfs[lDataset]
+            if(lDF.shape[0] > 0):
+                self.mPerformances["whole"][lDataset][lHorizonName] = self.compute_one_perf(lOriginalDF[lOriginalSignalColumn], lDF[lForecastColumn], lHorizonName);
+
         
     def computePerformances(self):
         # lTimer = tsutil.cTimer(("cPredictionIntervalsEstimator::computePerformances",
@@ -43,26 +112,25 @@ class cPredictionIntervalsEstimator:
         self.mOriginalSignal = self.mModel.mOriginalSignal;
         self.mHorizon = self.mModel.mTimeInfo.mHorizon;
         lTimeColumn = self.mTime;
-        lSignalColumn = self.mOriginalSignal;
+        lOriginalSignalColumn = self.mOriginalSignal;
         lForecastColumn = str(self.mOriginalSignal) + "_Forecast";
         df = self.mModel.mTrend.mSignalFrame.reset_index();
         N = df.shape[0];
-        (lOriginalFit, lOriginalForecast, lOriginalTest) = self.mModel.mTimeInfo.mSplit.cutFrame(df);
-        df1 = df[ [lTimeColumn , lSignalColumn] ];
+        original_cut_dfs = self.mModel.mTimeInfo.mSplit.cutFrame(df);
+        df1 = df[ [lTimeColumn , lOriginalSignalColumn] ];
         for h in range(0 , self.mHorizon):
             df2 = self.mModel.forecastOneStepAhead(df1, horizon_index = h+1, perf_mode = True);
             df2 = df2.head(N);
-            lHorizonName = lForecastColumn + "_" + str(h + 1);
-            (lFrameFit, lFrameForecast, lFrameTest) = self.mModel.mTimeInfo.mSplit.cutFrame(df2);
-            self.mFitPerformances[lHorizonName] = self.compute_one_perf(lOriginalFit[lSignalColumn], lFrameFit[lForecastColumn], lHorizonName);
+            forecast_cut_dfs = self.mModel.mTimeInfo.mSplit.cutFrame(df2);
+            self.compute_whole_perfs(original_cut_dfs, forecast_cut_dfs, h)
+            self.compute_detrended_perfs(original_cut_dfs, forecast_cut_dfs, h)
+            self.compute_deseasonalized_perfs(original_cut_dfs, forecast_cut_dfs, h)
+            self.compute_transformed_perfs(original_cut_dfs, forecast_cut_dfs, h)
             
-            self.mForecastPerformances[lHorizonName] = self.compute_one_perf(lOriginalForecast[lSignalColumn], lFrameForecast[lForecastColumn], lHorizonName);
-            if(lOriginalTest.shape[0] > 0):
-                self.mTestPerformances[lHorizonName] = self.compute_one_perf(lOriginalTest[lSignalColumn], lFrameTest[lForecastColumn], lHorizonName);
             df1 = df2[[lTimeColumn , lForecastColumn,
                        self.mModel.mTimeInfo.mRowNumberColumn,
                        self.mModel.mTimeInfo.mNormalizedTimeColumn]];
-            df1.columns = [lTimeColumn , lSignalColumn, self.mModel.mTimeInfo.mRowNumberColumn,
+            df1.columns = [lTimeColumn , lOriginalSignalColumn, self.mModel.mTimeInfo.mRowNumberColumn,
                            self.mModel.mTimeInfo.mNormalizedTimeColumn]
         # self.dump_detailed();
 
