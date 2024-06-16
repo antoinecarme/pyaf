0687bd49ebbfac17cb87ad7f371971f1c9dc22a5
Antoine CARME <antoine.carme@outlook.com>
2024-06-16 17:27:54 +0200

    Warning hunt

--- tests/bugs/issue_70/test_artificial_keep_all_seasonals_minute.py
+++ tests/bugs/issue_70/test_artificial_keep_all_seasonals_minute.py
@@ -7,7 +7,7 @@ import pyaf.Bench.TS_datasets as tsds
 
 b1 = tsds.generate_random_TS(N = 40000 , FREQ = 'min', seed = 0, trendtype = "constant", cycle_length = 24, transform = "None", sigma = 0.1, exog_count = 0);
 
-df = b1.mPastData
+df = b1.mPastData.copy()
 df[b1.mSignalVar] = df[b1.mName]
 
 lEngine = autof.cForecastEngine()
